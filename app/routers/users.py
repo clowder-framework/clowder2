4c6d07cee8d468208f2013737f210877d537ddf1
Luigi Marini <lmarini@illinois.edu>
2021-07-22 08:55:40 -0500

    Added `GET /users endpoint and fixed `GET /user/{user_id}` endpoint.

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,18 +1,29 @@
+from typing import List
+
 from bson import ObjectId
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from app.models.users import User
 
 router = APIRouter()
 
 
 @router.post('/users', response_model=User)
-async def save_me(body: User, request: Request):
+async def save_user(body: User, request: Request):
     res = await request.app.db["users"].insert_one(body.mongo())
     found = await request.app.db["users"].find_one({'_id': res.inserted_id})
     return User.from_mongo(found)
 
 
+@router.get("/users", response_model=List[User])
+async def get_users(request: Request, skip: int = 0, limit: int = 2):
+    users = []
+    for doc in await request.app.db["users"].find().skip(skip).limit(limit).to_list(length=limit):
+        users.append(doc)
+    return users
+
+
 @router.get("/users/{user_id}", response_model=User)
-async def read_root(user_id: str, request: Request):
-    found = await request.app.db["items"].find_one({'_id': user_id})
-    return User.from_mongo(found)
+async def get_user(user_id: str, request: Request):
+    if (user := await request.app.db["users"].find_one({"_id": ObjectId(user_id)})) is not None:
+        return user
+    raise HTTPException(status_code=404, detail=f"User {user_id} not found")
