525d046e06b5c56afa2f90484ed5842434b07d82
Chen Wang <cwang138@illinois.edu>
2021-08-04 13:37:54 -0500

    tab is working

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -2,21 +2,36 @@ import React, {useEffect, useState} from "react";
 import config from "../app.config";
 import TopBar from "./childComponents/TopBar";
 import Breadcrumbs from "./childComponents/BreadCrumb";
-import {
-	Tabs,
-	Tab,
-	AppBar,
-	Box,
-	Typography
-} from "@material-ui/core"
+import {AppBar, Box, Tab, Tabs, Typography} from "@material-ui/core"
+import {makeStyles} from "@material-ui/core/styles";
 
-export default function File(props){
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	appBar:{
+		background: "#FFFFFF",
+		boxShadow: "none",
+	},
+	tab:{
+		fontStyle: "normal",
+		fontWeight: "normal",
+		fontSize: "16px",
+		color: "#495057",
+		textTransform:"capitalize",
+	}
+}));
+
+export default function File(props) {
+	const classes = useStyles();
+
 	const {
 		listFileMetadata, fileMetadata,
 		listFileExtractedMetadata, fileExtractedMetadata,
 		listFileMetadataJsonld, fileMetadataJsonld,
 		listFilePreviews, filePreviews,
-		...other} = props;
+		...other
+	} = props;
 
 	const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
@@ -39,19 +54,20 @@ export default function File(props){
 			previewerScripts[0].parentNode.removeChild(previewerScripts[0]);
 		}
 
-		if (filePreviews.length > 0 && filePreviews[0].previews !== undefined){
+		if (filePreviews.length > 0 && filePreviews[0].previews !== undefined) {
 			let uniquePid = [];
 			// look at which previewer to load
-			filePreviews[0].previews.map((filePreview, index)=> {
+			filePreviews[0].previews.map((filePreview, index) => {
 
 				// do not attach same previewer twice
-				if (uniquePid.indexOf(filePreview["p_id"]) === -1){
+				if (uniquePid.indexOf(filePreview["p_id"]) === -1) {
 					uniquePid.push(filePreview["p_id"]);
 
 					// attach previwer jquery
 					const script = document.createElement("script");
 					script.className = "previewer-script";
-					script.src = `../public${filePreview["p_path"]}/${filePreview["p_main"]}`;;
+					script.src = `../public${filePreview["p_path"]}/${filePreview["p_main"]}`;
+					;
 					script.async = true;
 					document.body.appendChild(script);
 					return () => {
@@ -75,111 +91,114 @@ export default function File(props){
 
 	return (
 		<div>
-			<TopBar />
+			<TopBar/>
 			<div className="outer-container">
 				<Breadcrumbs filename={fileMetadata["filename"]}/>
 
 				{/*tabs*/}
-				<AppBar position="static">
-					<Tabs value={selectedTabIndex} onChange={handleTabChange} aria-label="file tabs">
-						<Tab label="Files" {...a11yProps(0)} />
-						<Tab label="Sections" {...a11yProps(1)} />
-						<Tab label="Metadata" {...a11yProps(2)} />
-						<Tab label="Extractions" {...a11yProps(3)} />
-						<Tab label="Comments" {...a11yProps(4)} />
-					</Tabs>
-				</AppBar>
-				<TabPanel value={selectedTabIndex} index={0}>
-					Previewer
-				</TabPanel>
-				<TabPanel value={selectedTabIndex} index={1}>
-					NA
-				</TabPanel>
-				<TabPanel value={selectedTabIndex} index={2}>
-					Metadata
-				</TabPanel>
-				<TabPanel value={selectedTabIndex} index={2}>
-					Extractions
-				</TabPanel>
-				<TabPanel value={selectedTabIndex} index={2}>
-					Comments
-				</TabPanel>
+				<div className="inner-container">
+					<AppBar className={classes.appBar} position="static">
+						<Tabs value={selectedTabIndex} onChange={handleTabChange} aria-label="file tabs">
+							<Tab className={classes.tab} label="Files" {...a11yProps(0)} />
+							<Tab className={classes.tab} label="Sections" {...a11yProps(1)} />
+							<Tab className={classes.tab} label="Metadata" {...a11yProps(2)} />
+							<Tab className={classes.tab} label="Extractions" {...a11yProps(3)} />
+							<Tab className={classes.tab} label="Comments" {...a11yProps(4)} />
+						</Tabs>
+					</AppBar>
+					<TabPanel value={selectedTabIndex} index={0}>
+						<h4 id="preview">previewer</h4>
+						{
+							filePreviews.length > 0 && filePreviews[0].previews !== undefined
+								?
+								filePreviews[0].previews.map((filePreview, index) => {
+									const Configuration = {};
+									Configuration.tab = `#previewer_${filePreviews[0]["file_id"]}_${index}`;
+									Configuration.url = `${config.hostname}${filePreview["pv_route"]}?superAdmin=true`;
+									Configuration.fileid = filePreview["pv_id"];
+									Configuration.previewer = `/public${filePreview["p_path"]}/`;
+									Configuration.fileType = filePreview["pv_contenttype"];
+									Configuration.APIKEY = config.apikey;
+									Configuration.authenticated = true;
+									// Configuration.metadataJsonld = fileMetadataJsonld;
 
-				<select name="files" id="files" onChange={(e)=>{ selectFile(e.target.value);}} defaultValue="select">
-					<option value="select" disabled>Select A File</option>
-					<option value="60ee082d5e0e3ff9d746b5fc">Text File</option>
-					<option value="59933ae8e4b04cf488f47aba">PDF</option>
-					<option value="5d974f435e0eb9edf7b3cf00">Audio</option>
-					<option value="59933ae9e4b04cf488f47b48">Video</option>
-					<option value="576b0b1ce4b0e899329e8553">Image</option>
-					<option value="60ee08325e0e3ff9d746bc57">Three D</option>
-				</select>
-				<h4>metadata</h4>
-				<div>
-					<h1>
-						{fileMetadata["filename"]}
-					</h1>
-					<p>{fileMetadata["content-type"]}</p>
-					<p>{fileMetadata["data-created"]}</p>
-					<p>{fileMetadata["authorId"]}</p>
-					<p>{fileMetadata["filedescription"]}</p>
-					<p>{fileMetadata["id"]}</p>
-					<p>{fileMetadata["size"]}</p>
-					<p>{fileMetadata["status"]}</p>
-					<p>{fileMetadata["thumbnail"]}</p>
-				</div>
-				<h4>metadata jsonld</h4>
-				<div>
-					{
-						fileMetadataJsonld.map((item) => {
-							return Object.keys(item["content"]).map((key) => {
-									return (<p>{key} - {JSON.stringify(item["content"][key])}</p>);
-								}
-							);
-						})
-					}
-				</div>
-				<h4 id="preview">previewer</h4>
-				{
-					filePreviews.length > 0 && filePreviews[0].previews !== undefined
-						?
-						filePreviews[0].previews.map((filePreview, index)=> {
-							const Configuration = {};
-							Configuration.tab = `#previewer_${filePreviews[0]["file_id"]}_${index}`;
-							Configuration.url = `${config.hostname}${filePreview["pv_route"]}?superAdmin=true`;
-							Configuration.fileid = filePreview["pv_id"];
-							Configuration.previewer = `/public${filePreview["p_path"]}/`;
-							Configuration.fileType = filePreview["pv_contenttype"];
-							Configuration.APIKEY=config.apikey;
-							Configuration.authenticated = true;
-							// Configuration.metadataJsonld = fileMetadataJsonld;
+									let previewId = filePreview["p_id"].replace(" ", "-").toLowerCase();
+									return (<div className={`configuration ${previewId}`}
+												 data-configuration={JSON.stringify(Configuration)}>
+										<div id={Configuration.tab.slice(1)}></div>
+									</div>);
+								})
+								:
+								<></>
+						}
+					</TabPanel>
+					<TabPanel value={selectedTabIndex} index={1}>
+						NA
+					</TabPanel>
+					<TabPanel value={selectedTabIndex} index={2}>
+						<h4>metadata</h4>
+						<div>
+							<h1>
+								{fileMetadata["filename"]}
+							</h1>
+							<p>{fileMetadata["content-type"]}</p>
+							<p>{fileMetadata["data-created"]}</p>
+							<p>{fileMetadata["authorId"]}</p>
+							<p>{fileMetadata["filedescription"]}</p>
+							<p>{fileMetadata["id"]}</p>
+							<p>{fileMetadata["size"]}</p>
+							<p>{fileMetadata["status"]}</p>
+							<p>{fileMetadata["thumbnail"]}</p>
+						</div>
+						<h4>metadata jsonld</h4>
+						<div>
+							{
+								fileMetadataJsonld.map((item) => {
+									return Object.keys(item["content"]).map((key) => {
+											return (<p>{key} - {JSON.stringify(item["content"][key])}</p>);
+										}
+									);
+								})
+							}
+						</div>
+					</TabPanel>
+					<TabPanel value={selectedTabIndex} index={3}>
+						Extractions
+					</TabPanel>
+					<TabPanel value={selectedTabIndex} index={4}>
+						Comments
+					</TabPanel>
 
-							let previewId = filePreview["p_id"].replace(" ","-").toLowerCase();
-							return (<div className={`configuration ${previewId}`} data-configuration={JSON.stringify(Configuration)}>
-								<div id={Configuration.tab.slice(1)}></div>
-							</div>);
-						})
-						:
-						<></>
-				}
+					<select name="files" id="files" onChange={(e) => { selectFile(e.target.value);}}
+							defaultValue="select">
+						<option value="select" disabled>Select A File</option>
+						<option value="60ee082d5e0e3ff9d746b5fc">Text File</option>
+						<option value="59933ae8e4b04cf488f47aba">PDF</option>
+						<option value="5d974f435e0eb9edf7b3cf00">Audio</option>
+						<option value="59933ae9e4b04cf488f47b48">Video</option>
+						<option value="576b0b1ce4b0e899329e8553">Image</option>
+						<option value="60ee08325e0e3ff9d746bc57">Three D</option>
+					</select>
+				</div>
 			</div>
+
 
 		</div>
 	);
 }
 
 function TabPanel(props) {
-	const { children, selectedTabIndex, index, ...other } = props;
+	const {children, value, index, ...other} = props;
 
 	return (
 		<div
 			role="tabpanel"
-			hidden={selectedTabIndex !== index}
+			hidden={value !== index}
 			id={`file-tabpanel-${index}`}
 			aria-labelledby={`file-tab-${index}`}
 			{...other}
 		>
-			{selectedTabIndex === index && (
+			{value === index && (
 				<Box p={3}>
 					<Typography>{children}</Typography>
 				</Box>
