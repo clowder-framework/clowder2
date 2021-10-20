ae8d2ce79e0859443c8b14630d53e39b91b7fc88
Chen Wang <cwang138@illinois.edu>
2021-10-20 14:49:00 -0500

    add cors for frontend development (#5)

--- app/main.py
+++ app/main.py
@@ -15,11 +15,28 @@ from mongoengine import connect
 
 from pydantic import BaseModel
 
+from fastapi.middleware.cors import CORSMiddleware
+
 auth_handler = AuthHandler()
 
 app = FastAPI(dependencies=[Depends(get_query_token)])
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(users.router)
 app.include_router(datasets.router)
