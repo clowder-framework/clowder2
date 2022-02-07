34aac005092c6edc05f7bc76cce29f6900084ead
Max Burnette <mburnet2@illinois.edu>
2022-02-07 15:31:20 -0600

    Update files.py

--- app/models/files.py
+++ app/models/files.py
@@ -6,22 +6,18 @@ from typing import List
 from app.models.mongomodel import MongoModel
 from app.models.pyobjectid import PyObjectId
 
-
-class FileVersion(BaseModel):
-    id: str = "_NA_"
+# MiniUser - id, name, email, gravatar (comes with email)
+class FileVersion(MongoModel):
+    id: str = "1"
+    file_id: str = "N/A"  # when v1 is initialized, this won't exist yet
     creator: PyObjectId = Field(default_factory=PyObjectId)
     created: datetime = Field(default_factory=datetime.utcnow)
 
 
 class ClowderFile(MongoModel):
+    name: str = "N/A"
     creator: PyObjectId = Field(default_factory=PyObjectId)
-    name: str = "_NA_"
     created: datetime = Field(default_factory=datetime.utcnow)
-    modified: datetime = Field(default_factory=datetime.utcnow)
-    versions: List[FileVersion] = []
+    version: str = "N/A"  # Minio version ID (if more than one version)
     views: int = 0
     downloads: int = 0
-
-
-class MongoFile(DynamicDocument):
-    pass
