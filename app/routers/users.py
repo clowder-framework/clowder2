bcd849ea40168b7615e5bb9a29d6a355eb6fdd95
Luigi Marini <lmarini@illinois.edu>
2022-02-24 16:50:12 -0600

    Users endpoints should return UserOut instead of UserDB.

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,17 +1,17 @@
 from typing import List
 
 from bson import ObjectId
-from fastapi import APIRouter, Request, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends
 from pymongo import MongoClient
 
 from app import dependencies
-from app.models.users import UserDB, UserIn
+from app.models.users import UserDB, UserIn, UserOut
 from passlib.hash import bcrypt
 
 router = APIRouter()
 
 
-@router.post("", response_model=UserDB)
+@router.post("", response_model=UserOut)
 async def save_user(userIn: UserIn, db: MongoClient = Depends(dependencies.get_db)):
     hashed_password = bcrypt.hash(userIn.password)
     userDB = UserDB(**userIn.dict(), hashed_password=hashed_password)
@@ -20,7 +20,7 @@ async def save_user(userIn: UserIn, db: MongoClient = Depends(dependencies.get_d
     return UserDB.from_mongo(found).dict(exclude={"create_at"})
 
 
-@router.get("", response_model=List[UserDB])
+@router.get("", response_model=List[UserOut])
 async def get_users(
     db: MongoClient = Depends(dependencies.get_db), skip: int = 0, limit: int = 2
 ):
@@ -30,14 +30,14 @@ async def get_users(
     return users
 
 
-@router.get("/{user_id}", response_model=UserDB)
+@router.get("/{user_id}", response_model=UserOut)
 async def get_user(user_id: str, db: MongoClient = Depends(dependencies.get_db)):
     if (user := await db["users"].find_one({"_id": ObjectId(user_id)})) is not None:
         return UserDB.from_mongo(user)
     raise HTTPException(status_code=404, detail=f"User {user_id} not found")
 
 
-@router.get("/username/{name}", response_model=UserDB)
+@router.get("/username/{name}", response_model=UserOut)
 async def get_user_by_name(name: str, db: MongoClient = Depends(dependencies.get_db)):
     if (user := await db["users"].find_one({"name": name})) is not None:
         return UserDB.from_mongo(user)
