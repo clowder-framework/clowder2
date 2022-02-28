b23395e8c675e1b6caae239ab145bca136104372
Luigi Marini <lmarini@illinois.edu>
2022-02-28 11:28:03 -0600

    Check user is logged in on all routers minus authentication ones.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import (
@@ -11,6 +11,7 @@ from app.routers import (
     folders,
 )
 from app.config import settings
+from app.routers.authentication import auth_handler
 
 app = FastAPI(
     title=settings.APP_NAME, openapi_url=f"{settings.API_V2_STR}/openapi.json"
@@ -26,13 +27,31 @@ app.add_middleware(
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(files.router, prefix="/files", tags=["files"])
-api_router.include_router(datasets.router, prefix="/datasets", tags=["datasets"])
 api_router.include_router(
-    collections.router, prefix="/collections", tags=["collections"]
+    files.router,
+    prefix="/files",
+    tags=["files"],
+    dependencies=[Depends(auth_handler.auth_wrapper)],
+)
+api_router.include_router(
+    datasets.router,
+    prefix="/datasets",
+    tags=["datasets"],
+    dependencies=[Depends(auth_handler.auth_wrapper)],
+)
+api_router.include_router(
+    collections.router,
+    prefix="/collections",
+    tags=["collections"],
+    dependencies=[Depends(auth_handler.auth_wrapper)],
 )
 api_router.include_router(authentication.router, tags=["login"])
-api_router.include_router(folders.router, prefix="/folders", tags=["folders"])
+api_router.include_router(
+    folders.router,
+    prefix="/folders",
+    tags=["folders"],
+    dependencies=[Depends(auth_handler.auth_wrapper)],
+)
 
 app.include_router(api_router, prefix=settings.API_V2_STR)
 
