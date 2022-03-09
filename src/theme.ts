d8f791a59e4c36cf00a27cd48a55078d5ba12717
Luigi Marini <lmarini@illinois.edu>
2022-03-09 11:22:11 -0600

    Turned background to gray and changed colors of dataset tiles.

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,11 @@ import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
 const theme = createTheme({
+	palette: {
+		background: {
+			default: "#F8F8F8"
+		}
+	}
 });
 
 export default theme;
