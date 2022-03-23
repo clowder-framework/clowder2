2ba3a9a49232b2cedb1a066e7431e7dfddf79795
Max Burnette <mburnet2@illinois.edu>
2022-03-23 09:38:13 -0500

    Initial stub

---
+++ app/models/metadata.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from typing import Optional
+from enum import Enum
+
+from pydantic import Field, validator
+
+from app.models.mongomodel import MongoModel
+from app.models.pyobjectid import PyObjectId
+from app.models.users import UserOut
+
+
+class ExtractorOut(MongoModel):
+    pass
+
+
+class MetadataField(MongoModel):
+    name: str
+    type: str = "str"  # str,int,float,bool
+    allow_many: bool = False  # whether a list[type] is acceptable
+
+
+class MetadataDefinition(MongoModel):
+    """Example: map
+    fields: (lon-float, lat-float)
+    those specify required fields, additional fields OK
+    fields used to build GUI for adding this type"""
+
+    name: str
+    description: Optional[str]
+    context: Optional[dict]  # https://json-ld.org/spec/latest/json-ld/#the-context
+    context_url: Optional[str]  # single URL applying to contents
+    fields: List[MetadataField]
+
+
+class MetadataAgent(MongoModel):
+    creator: UserOut
+    extractor: Optional[ExtractorOut]
+    client: str = "frontend"  # frontend, api, widget?
+
+
+class MetadataBase(MongoModel):
+    file_id: Optional[PyObjectId]
+    file_version: Optional[PyObjectId]
+    dataset_id: Optional[PyObjectId]
+    # does metadata need a version? or only when file bytes updated do we create new md document?
+    agent: MetadataAgent
+    context: Optional[dict]  # https://json-ld.org/spec/latest/json-ld/#the-context
+    context_url: Optional[str]  # single URL applying to contents
+    definition: Optional[str]  # e.g.'map' used for validation
+    contents: dict = {}
+    key: Optional[str]  # can be arbitrary
+
+    @validator("context")
+    def contexts_are_valid(self, v):
+        if False:
+            raise ValueError("Problem with context.")
+        return v
+
+    @validator("context_url")
+    def context_url_is_valid(self, v):
+        if False:
+            raise ValueError("Problem with context.")
+        return v
+
+
+class MetadataIn(MetadataBase):
+    pass
+
+
+class MetadataDB(MetadataBase):
+
+    created: datetime = Field(default_factory=datetime.utcnow)
+
+
+class MetadataOut(MetadataDB):
+    pass
