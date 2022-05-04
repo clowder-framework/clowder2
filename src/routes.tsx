e8cf3d6ee1698e91e7d7d6345f71bc3744c8197b
Luigi Marini <lmarini@illinois.edu>
2022-05-04 10:55:16 -0500

    Removed unused code.

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,8 +4,7 @@ import {Route, Navigate, Routes, BrowserRouter} from "react-router-dom";
 import {Dashboard} from "./components/Dashbard";
 import {Dataset as DatasetComponent} from "./components/datasets/Dataset";
 import {File as FileComponent} from "./components/files/File";
-// import {Login as LoginComponent} from "./components/auth/Login";
-// import {Logout as LogoutComponent} from "./components/auth/Logout";
+
 import {Register as RegisterComponent} from "./components/auth/Register";
 import {Auth as AuthComponent} from "./components/auth/Auth";
 import {RedirectLogin as RedirectLoginComponent} from "./components/auth/RedirectLogin";
@@ -28,8 +27,6 @@ const AppRoutes = (
 			<Route path="/" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
 			<Route path="/datasets/:datasetId" element={<PrivateRoute><DatasetComponent/></PrivateRoute>} />
 			<Route path="/files/:fileId" element={<PrivateRoute><FileComponent/></PrivateRoute>} />
-			{/*<Route path="/login" element={<LoginComponent/>} />*/}
-			{/*<Route path="/logout" element={<LogoutComponent/>} />*/}
 			<Route path="/register" element={<RegisterComponent/>} />
 			<Route path="/auth/login" element={<RedirectLoginComponent/>} />
 			<Route path="/auth/logout" element={<RedirectLogoutComponent/>} />
