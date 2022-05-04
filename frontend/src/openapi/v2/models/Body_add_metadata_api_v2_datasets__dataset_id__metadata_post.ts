f636102edb07d04019ba845f933ac45ba0c3f339
Chen Wang <cwang138@illinois.edu>
2022-05-04 14:12:25 -0500

    codegen new models

---
+++ frontend/src/openapi/v2/models/Body_add_metadata_api_v2_datasets__dataset_id__metadata_post.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { MetadataIn } from './MetadataIn';
+
+export type Body_add_metadata_api_v2_datasets__dataset_id__metadata_post = {
+    in_metadata: MetadataIn;
+    extractor_info?: any;
+}
---
+++ frontend/src/openapi/v2/models/MetadataIn.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type MetadataIn = {
+    id?: string;
+    context?: any;
+    context_url?: string;
+    definition?: string;
+    contents: any;
+}
