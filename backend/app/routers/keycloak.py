c01e334ebc8180ece1325cc8c2204529268540cc
Chen Wang <cwang138@illinois.edu>
2022-05-10 10:14:33 -0500

    add a redirect registration endpoint

--- backend/app/routers/keycloak.py
+++ backend/app/routers/keycloak.py
@@ -26,6 +26,10 @@ from app.models.tokens import TokenDB
 router = APIRouter()
 security = HTTPBearer()
 
+@router.get("/register")
+async def register() -> RedirectResponse:
+    return RedirectResponse(settings.auth_register_url)
+
 
 @router.get("/login")
 async def login() -> RedirectResponse:
