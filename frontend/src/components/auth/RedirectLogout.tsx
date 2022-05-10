93f43eede382003636fab3eb722e43aad2e0fe2d
Chen Wang <cwang138@illinois.edu>
2022-05-10 10:53:22 -0500

    configure the topbar to show correct login/register/logout

--- frontend/src/components/auth/RedirectLogout.tsx
+++ frontend/src/components/auth/RedirectLogout.tsx
@@ -1,14 +1,25 @@
 import React, {useEffect} from "react";
-import {logoutHelper} from "../../actions/user";
+import {logout} from "../../actions/user";
+import TopBar from "../navigation/TopBar";
+import {useDispatch} from "react-redux";
 
 export const RedirectLogout = (): JSX.Element => {
+
+	const dispatch = useDispatch();
+	const logUserOut = () => dispatch(logout());
+
 	useEffect(() => {
-		logoutHelper().then(res => {
-			console.log(res);
-		});
+		logUserOut();
 	}, []);
 
 	return (
-		<div>Logged Out!</div>
+		<div>
+			<TopBar/>
+			<div className="outer-container">
+				<p>you have logged out.</p>
+			</div>
+
+		</div>
+
 	)
 }
