a451f4514460f5ea560e97d0dbaf149a5dbeab6f
Chen Wang <cwang138@illinois.edu>
2022-05-12 14:32:52 -0500

    update the dev script to customize the theme

--- backend/docker-dev.sh
+++ backend/docker-dev.sh
@@ -2,6 +2,10 @@
 if [ $1 == "up" ]
 then
   docker-compose -f docker-compose.dev.yml up -d
+  # need to add volume so we can copy the clowder theme into the right place inside the container
+  echo "keycloak container id:" $(docker ps -aqf "name=keycloak")
+  docker cp ./keycloak/clowder-theme/. $(docker ps -aqf "name=keycloak"):/opt/jboss/keycloak/themes/clowder-theme
+  docker restart $(docker ps -aqf "name=keycloak")
 fi
 if [ $1 == "down" ]
 then
