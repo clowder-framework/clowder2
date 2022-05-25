7fb0d243140976e183ae6ba4e9eb073a064cb41b
toddn <tcnichol@illinois.edu>
2022-05-25 10:17:38 -0500

    not using file twice

--- backend/app/routers/datasets.py
+++ backend/app/routers/datasets.py
@@ -371,7 +371,7 @@ async def download_dataset(
             file_name = file.name
             file_id = str(file.id)
             try:
-                if (file := await db["files"].find_one({"_id": ObjectId(file_id)})) is not None:
+                if (current_file := await db["files"].find_one({"_id": ObjectId(file_id)})) is not None:
                     content = fs.get_object(settings.MINIO_BUCKET_NAME, file_id)
                     data = str(content.data)
                     z.writestr(file_name, data)
