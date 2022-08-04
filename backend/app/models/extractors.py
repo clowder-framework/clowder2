d489d4fe5609c38825fec60b2af3ba277e07be84
toddn <tcnichol@illinois.edu>
2022-08-04 16:17:53 -0500

    adding new fields

--- backend/app/models/extractors.py
+++ backend/app/models/extractors.py
@@ -1,13 +1,35 @@
 from datetime import datetime
 from pydantic import Field
+from typing import Optional, List
 from app.models.mongomodel import MongoModel
 
+
+class Repository(MongoModel):
+    repository_type: str = 'git'
+    repository_url: str = ''
+
+class ExtractionProcessTriggers(MongoModel):
+    dataset: List[str] = []
+    file: List[str] = []
+    metadata: List[str] = []
 
 class ExtractorIdentifier(MongoModel):
     name: str
     version: float = 1.0
     updated: datetime = Field(default_factory=datetime.utcnow)
     author: str
+    contributors: List[str] = []
+    # contexts: str
+    repository: Repository
+    external_services: List[str]
+    libraries: List[str]
+    bibtex: List[str]
+    maturity: str = "Development"
+    default_labels: List[str] = []
+    process: ExtractionProcessTriggers = ExtractionProcessTriggers()
+    categories: List[str]
+    # parameters: str
+
 
 
 class ExtractorBase(ExtractorIdentifier):
