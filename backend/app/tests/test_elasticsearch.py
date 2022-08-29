45fe2941a42560b87417d32490c3e98d1e0814b6
Dipannita Dey <ddey2@illinois.edu>
2022-08-29 12:15:43 -0500

    Added config to run Elasticsearch to docker Added basic implementation to index a file and unit test to cover the same
    Signed-off-by: Dipannita Dey <ddey2@illinois.edu>

---
+++ backend/app/tests/test_elasticsearch.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime
+
+from app.elasticsearch.connect import connect_elasticsearch, create_index, insert_record, search_index, delete_index
+
+dummy_index_name = 'dummy_file'
+dummy_record = {'name': 'test',
+            'creator': 'xyz',
+            'created': datetime.now(),
+            'download': 0
+          }
+dummy_query = {
+  "query": {
+      "match": {
+          "name": "test"
+      }
+  }
+}
+
+def test_elasticsearch():
+    es = connect_elasticsearch()
+    if es is not None:
+        create_index(es, dummy_index_name)
+        insert_record(es, dummy_index_name, dummy_record)
+        time.sleep(5)
+        result = search_index(es, dummy_index_name, dummy_query)
+        assert result['hits']['hits'][0]['_source']['creator'] == 'xyz'
+        delete_index(es, dummy_index_name)
