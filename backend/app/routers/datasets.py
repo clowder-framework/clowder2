d0d7e38d6873fd7ead71fb17e6958bb3a42d3bc2
Luigi Marini <lmarini@illinois.edu>
2022-10-03 16:33:59 -0500

    Default sort dataset list by creation date descending.

--- backend/app/routers/datasets.py
+++ backend/app/routers/datasets.py
@@ -8,6 +8,7 @@ import zipfile
 from collections.abc import Mapping, Iterable
 from typing import List, Optional
 
+import pymongo
 from bson import ObjectId
 from bson import json_util
 from fastapi import APIRouter, HTTPException, Depends, File, UploadFile, Response
@@ -186,6 +187,7 @@ async def get_datasets(
         for doc in (
             await db["datasets"]
             .find({"author.email": user_id})
+            .sort([('created', pymongo.DESCENDING)])
             .skip(skip)
             .limit(limit)
             .to_list(length=limit)
@@ -193,7 +195,7 @@ async def get_datasets(
             datasets.append(DatasetOut.from_mongo(doc))
     else:
         for doc in (
-            await db["datasets"].find().skip(skip).limit(limit).to_list(length=limit)
+            await db["datasets"].find().sort([('created', pymongo.DESCENDING)]).skip(skip).limit(limit).to_list(length=limit)
         ):
             datasets.append(DatasetOut.from_mongo(doc))
     return datasets
