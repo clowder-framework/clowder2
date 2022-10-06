cef2aadc05df6b49edac8354ab971c85b21b3901
toddn <tcnichol@illinois.edu>
2022-10-06 13:27:35 -0500

    delete test

--- backend/app/test.py
+++
@@ -1,8 +0,0 @@
-import time
-
-if __name__ == "__main__":
-    with open('testfile.txt') as f:
-        for i in range(0, 10):
-            line = 'line number ' + str(i) + '\n'
-            f.write(line)
-    print('done')
