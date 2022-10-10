93a0bd13b79e23eb2e445e40440e4c8b21b6259d
Dipannita <ddey2@illinois.edu>
2022-10-10 15:23:37 -0500

    Added indexing for dataset and added documentation #89 (#109)
    * Added indexing for dataset and added documentation
    
    Signed-off-by: Dipannita Dey <ddey2@illinois.edu>
    
    * fixing black tets
    
    * Fixed doc format and added endpoint to expose search
    
    * Addressed comments and added logic to abort while uploading file/dataset if any of the service is not available
    
    * 90 research reactivesearch working with elasticsearch (#126)
    
    * install reactivesearch
    
    * enable cors
    
    * cors; correct usage of reactivesearch
    
    * correctly configure reactive search
    
    * basic demo of reactivesearch
    
    * tmp
    
    * Added indexing for dataset and added documentation
    
    Signed-off-by: Dipannita Dey <ddey2@illinois.edu>
    
    * fixing black tets
    
    * Fixed doc format and added endpoint to expose search
    
    * add search endponit to router
    
    * search_index see hits but cannot return it successfully yet
    
    * proxy is working
    
    * add authentication
    
    * split into search file and search dataset
    
    * add route protection
    
    * keyword field cannot be directly query
    
    * Fixed elasticsearch pytest to use msearch.
    
    * switch back to keyword for filtering capability
    
    * typo
    
    Signed-off-by: Dipannita Dey <ddey2@illinois.edu>
    Co-authored-by: Dipannita Dey <ddey2@illinois.edu>
    Co-authored-by: Luigi Marini <lmarini@illinois.edu>
    
    * Black formatting.
    
    * Fixed method name typo.
    
    * Updated package-lock.json.
    
    Signed-off-by: Dipannita Dey <ddey2@illinois.edu>
    Co-authored-by: Luigi Marini <lmarini@illinois.edu>
    Co-authored-by: Chen Wang <cwang138@illinois.edu>

--- backend/app/tests/test_elastic_search.py
+++ backend/app/tests/test_elastic_search.py
@@ -1,30 +1,89 @@
+import json
 import time
 from datetime import datetime
 
-from app.elastic_search.connect import (
+from app.config import settings
+from app.search.config import indexSettings
+from app.search.connect import (
     connect_elasticsearch,
     create_index,
     insert_record,
     search_index,
     delete_index,
+    delete_document_by_id,
 )
 
-dummy_index_name = "dummy_file"
-dummy_record = {
-    "name": "test",
+dummy_file_index_name = "dummy_file"
+dummy_dataset_index_name = "dummy_dataset"
+
+dummy_file_record = {
+    "name": "test file",
     "creator": "xyz",
     "created": datetime.now(),
     "download": 0,
 }
-dummy_query = {"match": {"name": "test"}}
+dummy_dataset_record = {
+    "name": "test dataset",
+    "description": "dataset description",
+    "author": "abcd",
+    "created": datetime.now(),
+    "modified": 0,
+    "download": 0,
+}
 
 
-def test_elastic_search():
+def test_files():
     es = connect_elasticsearch()
     if es is not None:
-        create_index(es, dummy_index_name)
-        insert_record(es, dummy_index_name, dummy_record)
+        create_index(
+            es,
+            dummy_file_index_name,
+            settings.elasticsearch_setting,
+            indexSettings.file_mappings,
+        )
+        insert_record(es, dummy_file_index_name, dummy_file_record, 1)
         time.sleep(5)
-        result = search_index(es, dummy_index_name, dummy_query)
-        assert result["hits"]["hits"][0]["_source"]["creator"] == "xyz"
-        delete_index(es, dummy_index_name)
+        dummy_file_query = []
+        # header
+        dummy_file_query.append({"index": dummy_file_index_name})
+        # body
+        dummy_file_query.append({"query": {"match": {"creator": "xyz"}}})
+        file_query = ""
+        for each in dummy_file_query:
+            file_query += "%s \n" % json.dumps(each)
+
+        result = search_index(es, dummy_file_index_name, file_query)
+        assert (
+            result.body["responses"][0]["hits"]["hits"][0]["_source"]["name"]
+            == "test file"
+        )
+        delete_document_by_id(es, dummy_file_index_name, 1)
+        delete_index(es, dummy_file_index_name)
+
+
+def test_datasets():
+    es = connect_elasticsearch()
+    if es is not None:
+        create_index(
+            es,
+            dummy_dataset_index_name,
+            settings.elasticsearch_setting,
+            indexSettings.dataset_mappings,
+        )
+        insert_record(es, dummy_dataset_index_name, dummy_dataset_record, 1)
+        time.sleep(5)
+        dummy_dataset_query = []
+        # header
+        dummy_dataset_query.append({"index": dummy_dataset_index_name})
+        # body
+        dummy_dataset_query.append({"query": {"match": {"author": "abcd"}}})
+        dataset_query = ""
+        for each in dummy_dataset_query:
+            dataset_query += "%s \n" % json.dumps(each)
+        result = search_index(es, dummy_dataset_index_name, dataset_query)
+        assert (
+            result.body["responses"][0]["hits"]["hits"][0]["_source"]["author"]
+            == "abcd"
+        )
+        delete_document_by_id(es, dummy_dataset_index_name, 1)
+        delete_index(es, dummy_dataset_index_name)
