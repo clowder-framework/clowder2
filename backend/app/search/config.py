433b5ad6476fe1a749766397af6c94a9347101ff
Chen Wang <cwang138@illinois.edu>
2022-10-10 13:44:33 -0500

    typo

--- backend/app/search/config.py
+++ backend/app/search/config.py
@@ -3,7 +3,7 @@ class IndexSettings:
         "properties": {
             "name": {"type": "text"},
             "created": {"type": "date"},
-            "creator": {"type""keyword"},
+            "creator": {"type": "keyword"},
             "download": {"type": "long"},
         }
     }
@@ -12,7 +12,7 @@ class IndexSettings:
         "properties": {
             "name": {"type": "text"},
             "description": {"type": "text"},
-            "author": {"type""keyword"},
+            "author": {"type": "keyword"},
             "created": {"type": "date"},
             "modified": {"type": "date"},
             "download": {"type": "long"},
