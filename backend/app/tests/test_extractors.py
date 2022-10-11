9dd23aa12939aee5213e3f8a33233f5bb607c4f7
toddn <tcnichol@illinois.edu>
2022-10-11 13:09:21 -0500

    adding tests for extractors also adding a sample extractor_info.json file for registration purposes

---
+++ backend/app/tests/test_extractors.py
@@ -0,0 +1,42 @@
+import os
+from fastapi.testclient import TestClient
+from app.config import settings
+from app.models.pyobjectid import PyObjectId
+
+def test_register(client: TestClient, headers: dict):
+    with open('extractor_info.json', 'r') as f:
+        extractor_info = f.read()
+    response = client.post(
+        f"{settings.API_V2_STR}/extractors", json=extractor_info, headers=headers
+    )
+    assert response.json().get("id") is not None
+    assert response.status_code == 200
+
+def test_get_one(client: TestClient, headers: dict):
+    with open('extractor_info.json', 'r') as f:
+        extractor_info = f.read()
+    response = client.post(
+        f"{settings.API_V2_STR}/extractors", json=extractor_info, headers=headers
+    )
+    assert response.status_code == 200
+    assert response.json().get("id") is not None
+    extractor_id = response.json().get("id")
+    response = client.get(
+        f"{settings.API_V2_STR}/extractors/{extractor_id}", headers=headers
+    )
+    assert response.status_code == 200
+    assert response.json().get("id") is not None
+
+def test_delete(client: TestClient, headers: dict):
+    with open('extractor_info.json', 'r') as f:
+        extractor_info = f.read()
+    response = client.post(
+        f"{settings.API_V2_STR}/extractors", json=extractor_info, headers=headers
+    )
+    assert response.status_code == 200
+    assert response.json().get("id") is not None
+    extractor_id = response.json().get("id")
+    response = client.delete(
+        f"{settings.API_V2_STR}/extractors/{extractor_id}", headers=headers
+    )
+    assert response.status_code == 200
