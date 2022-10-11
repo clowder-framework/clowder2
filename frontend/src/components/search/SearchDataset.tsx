c4a4cb0d27de32684737e4bc59f80f7d3907035a
Chen Wang <cwang138@illinois.edu>
2022-10-11 12:26:11 -0500

    styling the search dataset page

--- frontend/src/components/search/SearchDataset.tsx
+++ frontend/src/components/search/SearchDataset.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {ReactiveBase, DataSearch, MultiList, SingleRange, ReactiveList, ReactiveComponent} from "@appbaseio/reactivesearch";
+import {ReactiveBase, DataSearch, ReactiveList, MultiDropdownList, SingleDropdownRange} from "@appbaseio/reactivesearch";
 import {parseDate} from "../../utils/common";
 
 import Cookies from "universal-cookie";
 import DatasetCard from "../datasets/DatasetCard";
-import {Grid} from "@mui/material";
+import {Box, Grid} from "@mui/material";
 import Layout from "../Layout";
 import theme from "../../theme";
 
@@ -21,8 +21,7 @@ const searchTheme = {
 		primaryColor: theme.palette.primary.main,
 		titleColor: theme.palette.secondary.dark,
 		alertColor: theme.palette.primary.dark,
-		borderColor: theme.palette.primary.main,
-		backgroundColor: theme.palette.primary.contrastText,
+		// backgroundColor: theme.palette.primary.contrastText
 	}
 };
 
@@ -31,79 +30,111 @@ export function SearchDataset() {
 	return (
 		<Layout>
 			<div className="outer-container">
-				<div className="inner-container">
-					<Grid container spacing={4}>
-						<Grid item xs>
-							<ReactiveBase
-							  url="http://localhost:8000/api/v2/elasticsearch"
-							  app="dataset"
-							  headers={{"Authorization": cookies.get("Authorization")}}
-							  // transformResponse={(elasticsearchResponse) => {
-							  // 	console.log(elasticsearchResponse)
-							  // 	// if (elasticsearchResponse.detail.error === "invalid_token"){
-							  // 	// 	console.log("token expired!");
-								// // }
-							  // }}
-							  theme={searchTheme}
-							>
-								{/*search*/}
-								<DataSearch
-									title="Search for Dataset"
-									componentId="searchbox"
-											autosuggest={true}
-											highlight={true}
-											queryFormat="or"
-											fuzziness={0}
-											debounce={100}
-											react={{ and: ["creatorfilter",
-													"downloadfilter",
-													"modifyfilter"]}}
-											// apply react to the filter
-											URLParams={true}
-											showFilter={true}
-											showClear
-											renderNoSuggestion="No suggestions found."
-											dataField={[
-												{field: "name", weight: 3},
-												{field: "description", weight: 2},
-												{field: "author.keyword", weight: 2},
-												]}
-											// placeholder="Search for Dataset"
-											innerClass={{
-												title: 'search-title',
-												input: 'search-input'
-											}}
-								/>
+				<Grid container spacing={4}>
+					<Grid item xs>
+						<ReactiveBase
+						  url="http://localhost:8000/api/v2/elasticsearch"
+						  app="dataset"
+						  headers={{"Authorization": cookies.get("Authorization")}}
+						  // transformResponse={(elasticsearchResponse) => {
+						  // 	console.log(elasticsearchResponse)
+						  // 	// if (elasticsearchResponse.detail.error === "invalid_token"){
+						  // 	// 	console.log("token expired!");
+							// // }
+						  // }}
+						  theme={searchTheme}
+						>
+							{/*search*/}
+							<DataSearch
+								title="Search for Dataset"
+								componentId="searchbox"
+										autosuggest={true}
+										highlight={true}
+										queryFormat="or"
+										fuzziness={0}
+										debounce={100}
+										react={{ and: ["creatorfilter",
+												"downloadfilter",
+												"modifyfilter"]}}
+										// apply react to the filter
+										URLParams={true}
+										showFilter={true}
+										showClear
+										renderNoSuggestion="No suggestions found."
+										dataField={[
+											{field: "name", weight: 3},
+											{field: "description", weight: 2},
+											{field: "author.keyword", weight: 2},
+											]}
+										// placeholder="Search for Dataset"
+										innerClass={{
+											title: "search-title",
+											input: "search-input",
+										}}
+							/>
+							{/*filters*/}
+							<Grid container spacing={2} sx={{marginBottom: "20px"}}>
+								<Grid item xs={12} sm={4} md={4} lg={4}>
+									<MultiDropdownList
+										componentId="creatorfilter"
+										dataField="author"
+										size={5}
+										sortBy="count"
+										showCount={true}
+										placeholder="Author: All"
+										innerClass={{
+											select: "filter-select"
+										}}
+									/>
+								</Grid>
+								<Grid item xs={12} sm={4} md={4} lg={4}>
+									<SingleDropdownRange
+										componentId="downloadfilter"
+										dataField="download"
+										data={[{ start: 0, label: "0 time and up" },
+											   { start: 10, label: "10 times and up" },
+											   { start: 100, label: "100 times and up" },
+										]}
+										placeholder="Download Times: All"
+										innerClass={{
+											select: "filter-select"
+										}}
+									/>
+								</Grid>
+								<Grid item xs={12} sm={4} md={4} lg={4}>
+									<SingleDropdownRange
+										componentId="modifyfilter"
+										dataField="modified"
+										data={[{ start: 0, label: "0 time and up" },
+											   { start: 10, label: "10 times and up" },
+											   { start: 100, label: "100 times and up" },
+										]}
+										placeholder="Modify Times: All"
+										innerClass={{
+											select: "filter-select"
+										}}
+									/>
+								</Grid>
+							</Grid>
 
-								{/*filters*/}
-								<MultiList componentId="creatorfilter" dataField="author" title="Filter by author" aggregationSize={5} showCheckbox/>
-								<SingleRange componentId="downloadfilter" dataField="download" title="Filter by Download Times"
-										   data={[{ start: 0, label: '0 time and up' },
-											   { start: 1, label: '1 times and up' }]}
-										   defaultValue="0 time and up" showRadio/>
-								<SingleRange componentId="modifyfilter" dataField="modified" title="Filter by Modified Times"
-												   data={[{ start: 0, label: '0 time and up' },
-													   { start: 1, label: '1 times and up' }]}
-												   defaultValue="0 time and up" showRadio/>
-								{/*result*/}
-								<ReactiveList componentId="results" dataField="_score" size={20} pagination={true}
-											  react={{
-												and: ["searchbox", "creatorfilter", "downloadfilter", "modifyfilter"]
-											  }}
-											  render={({ data }) => (
-													<Grid container spacing={2}>
-														{data.map((item) => (
-															<Grid item key={item._id} xs={12} sm={6} md={4} lg={3}>
-																<DatasetCard id={item._id} name={item.name} author={item.author}
-																			 created={parseDate(item.created)} description={item.description}/>
-															</Grid>
-															))}
-													</Grid>
-													)}/>
-							</ReactiveBase>
-						</Grid>
+							{/*result*/}
+							<ReactiveList componentId="results" dataField="_score" size={20} pagination={true}
+										  react={{
+											and: ["searchbox", "creatorfilter", "downloadfilter", "modifyfilter"]
+										  }}
+										  render={({ data }) => (
+												<Grid container spacing={2}>
+													{data.map((item) => (
+														<Grid item key={item._id} xs={12} sm={6} md={4} lg={3}>
+															<DatasetCard id={item._id} name={item.name} author={item.author}
+																		 created={parseDate(item.created)} description={item.description}/>
+														</Grid>
+														))}
+												</Grid>
+												)}/>
+						</ReactiveBase>
 					</Grid>
-				</div>
+				</Grid>
 			</div>
 		</Layout>
   );
