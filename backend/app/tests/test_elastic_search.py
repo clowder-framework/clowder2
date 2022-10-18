8d1c5507560502ea8b31dbf03a8891e19d24d0fd
Chen Wang <cwang138@illinois.edu>
2022-10-18 11:03:02 -0500

    black linting

--- backend/app/tests/test_elastic_search.py
+++ backend/app/tests/test_elastic_search.py
@@ -23,7 +23,7 @@ dummy_file_record = {
     "creator": "xyz",
     "created": datetime.now(),
     "download": 0,
-    "dataset_id": str(ObjectId('63458339aaecb776733354ea')),
+    "dataset_id": str(ObjectId("63458339aaecb776733354ea")),
     "folder_id": None,
     "bytes": 123456,
     "content_type": "application/json",
