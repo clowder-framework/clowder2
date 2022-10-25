40f983005e474b820a4c9589a86b9915ee45b936
Chen Wang <cwang138@illinois.edu>
2022-10-25 16:16:06 -0500

    fix minicssextractor plugin

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -50,7 +50,7 @@ export default {
 		}),
 
 		// Generate an external css file with a hash in the filename
-		new MiniCssExtractPlugin("[name].[contenthash].css"),
+		new MiniCssExtractPlugin({filename:"[name].[contenthash].css"}),
 
 		// Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
 		new HtmlWebpackPlugin({
