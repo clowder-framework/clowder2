c917d3d8d21cec83a5e10a55258a7b344172d67f
Luigi Marini <lmarini@illinois.edu>
2022-11-14 16:10:49 -0600

    Fixed extra bytes in files table.

--- frontend/src/components/files/FilesTableFileEntry.tsx
+++ frontend/src/components/files/FilesTableFileEntry.tsx
@@ -31,7 +31,7 @@ export function FilesTableFileEntry(props) {
 				<VersionChip versionNumber={file.version_num}/>
 			</TableCell>
 			<TableCell align="right">{parseDate(file.created)} by {file.creator.first_name} {file.creator.last_name}</TableCell>
-			<TableCell align="right">{prettyBytes(file.bytes)} bytes</TableCell>
+			<TableCell align="right">{prettyBytes(file.bytes)}</TableCell>
 			<TableCell align="right">{file.content_type}</TableCell>
 			<TableCell align="right"><FileMenu file={file}/></TableCell>
 		</TableRow>
