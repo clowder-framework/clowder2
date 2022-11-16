b5bf0e9d8835ed42f1477374174341c6bf393a38
Luigi Marini <lmarini@illinois.edu>
2022-11-16 14:42:21 -0600

    Removed extra white space around the dataset page.

--- frontend/src/components/datasets/Dataset.tsx
+++ frontend/src/components/datasets/Dataset.tsx
@@ -13,7 +13,6 @@ import {
 	Tabs,
 	Typography,
 } from "@mui/material";
-import {ClowderInput} from "../styledComponents/ClowderInput";
 import {ClowderButton} from "../styledComponents/ClowderButton";
 import {useParams, useSearchParams} from "react-router-dom";
 import {RootState} from "../../types/data";
@@ -116,11 +115,6 @@ export const Dataset = (): JSX.Element => {
 		setSelectedTabIndex(newTabIndex);
 	};
 
-	const handleDatasetDescriptionEdit = () => {
-		editDataset(about["id"], {"description": datasetDescription});
-		setEditDescriptionOpen(false);
-	};
-
 	const setMetadata = (metadata: any) => {
 		// TODO wrap this in to a function
 		setMetadataRequestForms(prevState => {
@@ -185,131 +179,127 @@ export const Dataset = (): JSX.Element => {
 			<ActionModal actionOpen={errorOpen} actionTitle="Something went wrong..." actionText={reason}
 						 actionBtnName="Report" handleActionBtnClick={handleErrorReport}
 						 handleActionCancel={handleErrorCancel}/>
-			<div className="outer-container">
-				<Grid container>
-					<Grid item xs={8} sx={{display: 'flex',  alignItems: 'center'}}>
-						<MainBreadcrumbs paths={paths}/>
-					</Grid>
-					<Grid item xs={4}>
-						<ActionsMenu datasetId={datasetId} folderId={folderId}/>
-					</Grid>
+			<Grid container>
+				<Grid item xs={8} sx={{display: 'flex', alignItems: 'center'}}>
+					<MainBreadcrumbs paths={paths}/>
 				</Grid>
-				<div className="inner-container">
-					<Grid container spacing={2}>
-						<Grid item xs={10}>
-							<Typography variant="h3" paragraph>{about["name"]}</Typography>
-							<Typography variant="body1" paragraph>{about["description"]}</Typography>
-							<Tabs value={selectedTabIndex} onChange={handleTabChange} aria-label="dataset tabs">
-								<Tab sx={tab} label="Files" {...a11yProps(0)} />
-								<Tab sx={tab} label="Metadata" {...a11yProps(1)} disabled={false}/>
-							</Tabs>
-							<TabPanel value={selectedTabIndex} index={0}>
-								<FilesTable datasetId={datasetId}/>
-							</TabPanel>
-							<TabPanel value={selectedTabIndex} index={1}>
-								{
-									enableAddMetadata ?
-										<>
-											<IconButton color="primary" aria-label="close"
-														onClick={() => {
-															setEnableAddMetadata(false);
-														}}
-														sx={{float: "right"}}
-											>
-												<CloseIcon/>
-											</IconButton>
-											<Button variant="contained" onClick={() => {
-												setOpenPopup(true);
-											}} sx={{mt: 1, mr: 1, "alignItems": "right"}}>
-												Add new metadata definition
-											</Button>
-											<EditMetadata resourceType="dataset" resourceId={datasetId}
-														  setMetadata={setMetadata}
-											/>
-											<Button variant="contained" onClick={handleMetadataUpdateFinish}
-													sx={{mt: 1, mr: 1}}>
-												Update
-											</Button>
-											<Button onClick={() => {
-												setEnableAddMetadata(false);
-											}}
-													sx={{mt: 1, mr: 1}}>
-												Cancel
-											</Button>
-											{
-												openPopup ?
-													<>
-														<Dialog open={openPopup} onClose={() => {
+				<Grid item xs={4}>
+					<ActionsMenu datasetId={datasetId} folderId={folderId}/>
+				</Grid>
+			</Grid>
+			<Grid container spacing={2}>
+				<Grid item xs={10}>
+					<Typography variant="h3" paragraph>{about["name"]}</Typography>
+					<Typography variant="body1" paragraph>{about["description"]}</Typography>
+					<Tabs value={selectedTabIndex} onChange={handleTabChange} aria-label="dataset tabs">
+						<Tab sx={tab} label="Files" {...a11yProps(0)} />
+						<Tab sx={tab} label="Metadata" {...a11yProps(1)} disabled={false}/>
+					</Tabs>
+					<TabPanel value={selectedTabIndex} index={0}>
+						<FilesTable datasetId={datasetId}/>
+					</TabPanel>
+					<TabPanel value={selectedTabIndex} index={1}>
+						{
+							enableAddMetadata ?
+								<>
+									<IconButton color="primary" aria-label="close"
+												onClick={() => {
+													setEnableAddMetadata(false);
+												}}
+												sx={{float: "right"}}
+									>
+										<CloseIcon/>
+									</IconButton>
+									<Button variant="contained" onClick={() => {
+										setOpenPopup(true);
+									}} sx={{mt: 1, mr: 1, "alignItems": "right"}}>
+										Add new metadata definition
+									</Button>
+									<EditMetadata resourceType="dataset" resourceId={datasetId}
+												  setMetadata={setMetadata}
+									/>
+									<Button variant="contained" onClick={handleMetadataUpdateFinish}
+											sx={{mt: 1, mr: 1}}>
+										Update
+									</Button>
+									<Button onClick={() => {
+										setEnableAddMetadata(false);
+									}}
+											sx={{mt: 1, mr: 1}}>
+										Cancel
+									</Button>
+									{
+										openPopup ?
+											<>
+												<Dialog open={openPopup} onClose={() => {
+													setOpenPopup(false);
+												}} fullWidth={true} maxWidth={"md"}>
+													<DialogTitle>Add new metadata definition</DialogTitle>
+													<DialogContent>
+														<DialogContentText>Please fill out the metadata
+															information here.</DialogContentText>
+														<CreateMetadataDefinition/>
+													</DialogContent>
+													<DialogActions>
+														<Button onClick={() => {
 															setOpenPopup(false);
-														}} fullWidth={true} maxWidth={"md"}>
-															<DialogTitle>Add new metadata definition</DialogTitle>
-															<DialogContent>
-																<DialogContentText>Please fill out the metadata
-																	information here.</DialogContentText>
-																<CreateMetadataDefinition/>
-															</DialogContent>
-															<DialogActions>
-																<Button onClick={() => {
-																	setOpenPopup(false);
-																}}>Cancel</Button>
-															</DialogActions>
-														</Dialog>
-													</>
-													: <></>
-											}
-										</>
-										:
-										<>
-											<Grid container spacing={2} sx={{"alignItems": "center"}}>
-												<Grid item xs={11} sm={11} md={11} lg={11} xl={11}>
-													<ClowderButton onClick={() => {
-														setEnableAddMetadata(true);
-													}}>
-														Add/Edit Metadata
-													</ClowderButton>
-												</Grid>
-											</Grid>
-											<DisplayMetadata updateMetadata={updateDatasetMetadata}
-															 deleteMetadata={deleteDatasetMetadata}
-															 resourceType="dataset" resourceId={datasetId}/>
-										</>
+														}}>Cancel</Button>
+													</DialogActions>
+												</Dialog>
+											</>
+											: <></>
+									}
+								</>
+								:
+								<>
+									<Grid container spacing={2} sx={{"alignItems": "center"}}>
+										<Grid item xs={11} sm={11} md={11} lg={11} xl={11}>
+											<ClowderButton onClick={() => {
+												setEnableAddMetadata(true);
+											}}>
+												Add/Edit Metadata
+											</ClowderButton>
+										</Grid>
+									</Grid>
+									<DisplayMetadata updateMetadata={updateDatasetMetadata}
+													 deleteMetadata={deleteDatasetMetadata}
+													 resourceType="dataset" resourceId={datasetId}/>
+								</>
 
-								}
-							</TabPanel>
-							<TabPanel value={selectedTabIndex} index={2}/>
-							<TabPanel value={selectedTabIndex} index={3}/>
-							<TabPanel value={selectedTabIndex} index={4}/>
-						</Grid>
-						<Grid item xs={2} justifyContent="center">
-							{
-								about !== undefined ?
-									<Box className="infoCard">
-										<Typography className="title">About</Typography>
-										<Typography className="content">
-											Owner: {about["author"]["first_name"]} {about["author"]["last_name"]}
-										</Typography>
-										<Typography className="content">Created
-											on: {parseDate(about["created"])}</Typography>
-										<Typography className="content">Modified
-											on: {parseDate(about["modified"])}</Typography>
-										{/*/!*TODO use this to get thumbnail*!/*/}
-										{/*<Typography className="content">Thumbnail: {about["thumbnail"]}</Typography>*/}
-										{/*<Typography className="content">Belongs to spaces: {about["authorId"]}</Typography>*/}
-										{/*/!*TODO not sure how to use this info*!/*/}
-										{/*<Typography className="content">Resource type: {about["resource_type"]}</Typography>*/}
-									</Box> : <></>
-							}
+						}
+					</TabPanel>
+					<TabPanel value={selectedTabIndex} index={2}/>
+					<TabPanel value={selectedTabIndex} index={3}/>
+					<TabPanel value={selectedTabIndex} index={4}/>
+				</Grid>
+				<Grid item xs={2} justifyContent="center">
+					{
+						about !== undefined ?
 							<Box className="infoCard">
-								<Typography className="title">Statistics</Typography>
-								<Typography className="content">Views: 10</Typography>
-								<Typography className="content">Last viewed: Jun 07, 2021 21:49:09</Typography>
-								<Typography className="content">Downloads: 0</Typography>
-								<Typography className="content">Last downloaded: Never</Typography>
-							</Box>
-						</Grid>
-					</Grid>
-				</div>
-			</div>
+								<Typography className="title">About</Typography>
+								<Typography className="content">
+									Owner: {about["author"]["first_name"]} {about["author"]["last_name"]}
+								</Typography>
+								<Typography className="content">Created
+									on: {parseDate(about["created"])}</Typography>
+								<Typography className="content">Modified
+									on: {parseDate(about["modified"])}</Typography>
+								{/*/!*TODO use this to get thumbnail*!/*/}
+								{/*<Typography className="content">Thumbnail: {about["thumbnail"]}</Typography>*/}
+								{/*<Typography className="content">Belongs to spaces: {about["authorId"]}</Typography>*/}
+								{/*/!*TODO not sure how to use this info*!/*/}
+								{/*<Typography className="content">Resource type: {about["resource_type"]}</Typography>*/}
+							</Box> : <></>
+					}
+					<Box className="infoCard">
+						<Typography className="title">Statistics</Typography>
+						<Typography className="content">Views: 10</Typography>
+						<Typography className="content">Last viewed: Jun 07, 2021 21:49:09</Typography>
+						<Typography className="content">Downloads: 0</Typography>
+						<Typography className="content">Last downloaded: Never</Typography>
+					</Box>
+				</Grid>
+			</Grid>
 		</Layout>
-	);
-};
+	)
+}
