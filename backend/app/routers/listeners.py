ccc818ba7e30f2cba23cbd8a7f6f985d61a381be
Chen Wang <cwang138@illinois.edu>
2022-12-13 17:36:17 -0600

    backend endpoint

--- backend/app/routers/listeners.py
+++ backend/app/routers/listeners.py
@@ -108,6 +108,33 @@ async def get_listeners(
     return listeners
 
 
+@router.get("/search", response_model=List[EventListenerOut])
+async def search_listeners(
+    user_id=Depends(get_user),
+    db: MongoClient = Depends(get_db),
+    text: str = "",
+    skip: int = 0,
+    limit: int = 2,
+):
+    """Search all Event Listeners in the db based on text.
+
+    Arguments:
+        text -- any text matching name or description
+        skip -- number of initial records to skip (i.e. for pagination)
+        limit -- restrict number of records to be returned (i.e. for pagination)
+    """
+    listeners = []
+    for doc in (
+        await db["listeners"].find({"$or":
+            [
+                {"name": "/"+text+"/i" },
+                {"description": "/"+text+"/i"}
+            ]}).skip(skip).limit(limit).to_list(length=limit)
+    ):
+        listeners.append(EventListenerOut.from_mongo(doc))
+    return listeners
+
+
 @router.put("/{listener_id}", response_model=EventListenerOut)
 async def edit_listener(
     listener_id: str,
