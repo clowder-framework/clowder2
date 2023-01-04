b9fedd96a7a91f96aa4cf06b8626f086fdcb938c
Chen Wang <cwang138@illinois.edu>
2023-01-04 10:29:58 -0600

    add stepper back

--- frontend/src/components/listeners/SubmitExtraction.tsx
+++ frontend/src/components/listeners/SubmitExtraction.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useDispatch} from "react-redux";
 import {
 	Box,
@@ -7,8 +7,11 @@ import {
 	Dialog,
 	DialogActions,
 	DialogContent,
-	DialogContentText,
-	DialogTitle
+	DialogTitle,
+	Step,
+	StepContent,
+	StepLabel,
+	Stepper, Typography
 } from "@mui/material";
 
 import {ListenerInfo} from "./ListenerInfo";
@@ -17,6 +20,7 @@ import {FormProps} from "@rjsf/core";
 import {submitFileExtractionAction} from "../../actions/file";
 import {submitDatasetExtractionAction} from "../../actions/dataset";
 import {Extractor} from "../../types/data";
+import {CreateMetadata} from "../metadata/CreateMetadata";
 
 type SubmitExtractionProps = {
 	fileId: string,
@@ -33,16 +37,29 @@ export default function SubmitExtraction(props: SubmitExtractionProps) {
 	const dispatch = useDispatch();
 
 	const submitFileExtraction =
-		(fileId: string | undefined, extractorName: string | undefined, requestBody:FormData) => dispatch(submitFileExtractionAction(fileId, extractorName, requestBody));
+		(fileId: string | undefined, extractorName: string | undefined, requestBody: FormData) => dispatch(submitFileExtractionAction(fileId, extractorName, requestBody));
 	const submitDatasetExtraction =
-		(datasetId: string | undefined, extractorName: string | undefined, requestBody:FormData) => dispatch(submitDatasetExtractionAction(datasetId, extractorName, requestBody));
+		(datasetId: string | undefined, extractorName: string | undefined, requestBody: FormData) => dispatch(submitDatasetExtractionAction(datasetId, extractorName, requestBody));
 	const onSubmit = (formData: FormData) => {
 		const extractorName = selectedExtractor.name
 		if (fileId === undefined && datasetId !== undefined) {
 			submitDatasetExtraction(datasetId, extractorName, formData);
+			handleNext();
 		} else if (fileId !== undefined) {
 			submitFileExtraction(fileId, extractorName, formData);
+			handleNext();
 		}
+	}
+
+	const [activeStep, setActiveStep] = useState(0);
+	const handleNext = () => {
+		setActiveStep((prevActiveStep) => prevActiveStep + 1);
+	};
+	const handleBack = () => {
+		setActiveStep((prevActiveStep) => prevActiveStep - 1);
+	}
+	const handleFinish = () => {
+		setActiveStep(0);
 	}
 
 	return (
@@ -51,42 +68,76 @@ export default function SubmitExtraction(props: SubmitExtractionProps) {
 			<Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth="sm">
 				<DialogTitle><ListenerInfo/></DialogTitle>
 				<DialogContent>
-					<DialogContentText>
-						Fill in the required extractor parameters
-					</DialogContentText>
-					{
-						selectedExtractor &&
-						selectedExtractor["properties"]
-						&& selectedExtractor["properties"]["parameters"]
-						&& selectedExtractor["properties"]["parameters"]["schema"] ?
-							<Container>
-								<Form
-									schema={{"properties": selectedExtractor["properties"]["parameters"]["schema"] as FormProps<any>["schema"]}}
-									onSubmit={({formData}) => {
-										onSubmit(formData);
-									}}>
-									<Box className="inputGroup">
-										<Button variant="contained" type="submit"
-												className="form-button-block"
-										>Submit</Button>
-									</Box>
-								</Form>
-							</Container>
-							:
-							<Container>
-								<Form
-									schema={{"properties": {}}}
-									onSubmit={({formData}) => {
-										onSubmit(formData);
-									}}>
-									<Box className="inputGroup">
-										<Button variant="contained" type="submit"
-												className="form-button-block"
-										>Submit</Button>
-									</Box>
-								</Form>
-							</Container>
-					}
+					<Stepper activeStep={activeStep} orientation="vertical">
+						{/*step 1 fill in parameters and submit extractions*/}
+						<Step key="submit">
+							<StepLabel>Submit Extractions</StepLabel>
+							<StepContent>
+								{
+									selectedExtractor &&
+									selectedExtractor["properties"]
+									&& selectedExtractor["properties"]["parameters"]
+									&& selectedExtractor["properties"]["parameters"]["schema"] ?
+										<Container>
+											<Form
+												schema={{"properties": selectedExtractor["properties"]["parameters"]["schema"] as FormProps<any>["schema"]}}
+												onSubmit={({formData}) => {
+													onSubmit(formData);
+												}}>
+												<Box className="inputGroup">
+													<Button variant="contained" type="submit"
+															className="form-button-block"
+													>Submit</Button>
+												</Box>
+											</Form>
+										</Container>
+										:
+										<Container>
+											<Form
+												schema={{"properties": {}}}
+												onSubmit={({formData}) => {
+													onSubmit(formData);
+												}}>
+												<Box className="inputGroup">
+													<Button variant="contained" type="submit"
+															className="form-button-block"
+													>Submit</Button>
+												</Box>
+											</Form>
+										</Container>
+								}
+							</StepContent>
+						</Step>
+						{/*step 2 status*/}
+						<Step key="status">
+							<StepLabel>Extraction Status</StepLabel>
+							<StepContent>
+								{/*buttons*/}
+								<Box sx={{ mb: 2 }}>
+									<Button variant="contained" onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+										Next
+									</Button>
+								</Box>
+							</StepContent>
+						</Step>
+						{/*step 2 results*/}
+						<Step key="results">
+							<StepLabel>Extracted Results</StepLabel>
+							<StepContent>
+								{/*buttons*/}
+								<Box sx={{ mb: 2 }}>
+									<>
+										<Button variant="contained" onClick={handleFinish} sx={{ mt: 1, mr: 1 }}>
+											Restart
+										</Button>
+										<Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+											Back
+										</Button>
+									</>
+								</Box>
+							</StepContent>
+						</Step>
+					</Stepper>
 				</DialogContent>
 				<DialogActions>
 					<Button onClick={handleClose}>Close</Button>
