1f62f76028533a5baadcff942ac4a6f9bdb51e75
Chen Wang <cwang138@illinois.edu>
2023-01-25 13:42:07 -0600

    281 css style change when using rjsf (#291)
    * write custom widget of text and select for rjsf form
    
    * use custom widget for dataset creation as well
    
    * black format

---
+++ frontend/src/components/styledComponents/ClowderRjsfTextWidget.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@material-ui/core";
+import React from "react";
+import {ClowderInput} from "./ClowderInput";
+import {ClowderInputLabel} from "./ClowderInputLabel";
+
+export const ClowderRjsfTextWidget = (item) => {
+	return (
+		<>
+			<ClowderInputLabel>{item.schema.title}</ClowderInputLabel>
+			<ClowderInput
+				value={item.value}
+				variant="outlined"
+				onChange={(e) => {
+					item.onChange(e.target.value);
+				}}
+			/>
+		</>
+	);
+};
