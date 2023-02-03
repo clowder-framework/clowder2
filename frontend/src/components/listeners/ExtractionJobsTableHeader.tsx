23577d965f1678ab02a22b1d3a148f92b59066d2
Chen Wang <cwang138@illinois.edu>
2023-02-03 10:45:50 -0600

    271 extractor catalog page on frontend with extractor history (#294)
    * temp
    
    * on select show listener info
    
    * add table to display job histories
    
    * wire all the components in
    
    * fix layout
    
    * write individual components
    
    * add endpoint needed for the history page
    
    * minor tweaks
    
    * codegen
    
    * filter by extractor name works
    
    * calendar date range doesn't work
    
    * datetime half work
    
    * default at null
    
    * switch extractor will switch the date
    
    * timezone is not correct
    
    * fix the date time range
    
    * add two more status
    
    * add refresh button; coordinate 2 filters
    
    ---------
    
    Co-authored-by: Max Burnette <mburnet2@illinois.edu>

---
+++ frontend/src/components/listeners/ExtractionJobsTableHeader.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Box from "@mui/material/Box";
+import {visuallyHidden} from "@mui/utils";
+import {Data, Order} from "./ExtractionJobs";
+
+interface EnhancedTableProps {
+	onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+	order: Order;
+	orderBy: string;
+	headCells: any;
+}
+
+export const EnhancedTableHead = (props: EnhancedTableProps) => {
+	const {order, orderBy, onRequestSort, headCells} = props;
+	const createSortHandler =
+		(property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+			onRequestSort(event, property);
+		};
+
+	return (
+		<TableHead>
+			<TableRow>
+				{headCells.map((headCell) => (
+					<TableCell
+						key={headCell.id}
+						align="left"
+						padding="normal"
+						sortDirection={orderBy === headCell.id ? order : false}
+					>
+						<TableSortLabel
+							active={orderBy === headCell.id}
+							direction={orderBy === headCell.id ? order : "asc"}
+							onClick={createSortHandler(headCell.id)}
+						>
+							{headCell.label}
+							{orderBy === headCell.id ? (
+								<Box component="span" sx={visuallyHidden}>
+									{order === "desc" ? "sorted descending" : "sorted ascending"}
+								</Box>
+							) : null}
+						</TableSortLabel>
+					</TableCell>
+				))}
+			</TableRow>
+		</TableHead>
+	);
+}
