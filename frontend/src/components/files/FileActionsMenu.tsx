3182eb67b9b17689f5c9a8cc5d0d415399cc9962
Aruna Parameswaran <aruna.parame@gmail.com>
2023-02-27 09:38:58 -0600

    Added new component for file actions (#352)
    * Added new component for file actions
    
    * Tested file download and file delete from File page
    
    * Verified after deleting file, page navigates back to main dataset page
    
    * Fixed version-not-updating error
    
    * Updated onSave sequence to first synchronously update the file and then subsequently call listVersions API to display on the frontend
    
    * Fixed lint issues by running eslint

---
+++ frontend/src/components/files/FileActionsMenu.tsx
@@ -0,0 +1,113 @@
+import {Button, Dialog, DialogTitle, ListItemIcon, ListItemText, Menu, MenuItem, Stack} from "@mui/material";
+import React, { useState } from "react";
+import {fileDeleted, fileDownloaded} from "../../actions/file";
+import {useDispatch, useSelector} from "react-redux";
+import {Download, MoreHoriz, Upload} from "@mui/icons-material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { ActionModal } from "../dialog/ActionModal";
+import { UpdateFile } from "./UpdateFile";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {useNavigate} from "react-router-dom";
+
+type FileActionsMenuProps = {
+    fileId: string | undefined,
+    filename: string | undefined,
+    datasetId: string | null
+}
+
+export const FileActionsMenu = (props: FileActionsMenuProps): JSX.Element => {
+	const {fileId, filename, datasetId} = props;
+    
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+    
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+    
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+    
+	// redux
+	const dispatch = useDispatch();
+    
+	const deleteFile = (fileId:string|undefined) => dispatch(fileDeleted(fileId));
+	const downloadFile = (fileId: string | undefined, filename: string | undefined) => dispatch(fileDownloaded(fileId, filename));
+    
+	const history = useNavigate();
+
+	const [confirmationOpen, setConfirmationOpen] = useState(false);
+    
+	const [updateFileOpen, setUpdateFileOpen] = useState(false);
+	const deleteSelectedFile = () => {
+		if (fileId) {
+			deleteFile(fileId);
+		}
+		setConfirmationOpen(false);
+
+		// Redirect back to main dataset page
+		history(`/datasets/${datasetId}`);
+	};
+
+	return (
+		<Stack direction="row"
+			justifyContent="flex-end"
+			alignItems="center"
+			spacing={0.5}>
+			<ActionModal actionOpen={confirmationOpen} actionTitle="Are you sure?"
+						 actionText="Do you really want to delete? This process cannot be undone."
+						 actionBtnName="Delete" handleActionBtnClick={deleteSelectedFile}
+						 handleActionCancel={() => { setConfirmationOpen(false);}}/>
+			<Dialog open={updateFileOpen} onClose={()=>{setUpdateFileOpen(false);}} fullWidth={true} aria-labelledby="form-dialog">
+				<DialogTitle id="form-dialog-title">Update File</DialogTitle>
+				<UpdateFile fileId={fileId} setOpen={setUpdateFileOpen}/>
+			</Dialog>
+			<Button variant="contained"
+				onClick={() => {
+					downloadFile(fileId, filename);
+					handleClose();
+				}} endIcon={<Download/>}>
+        Download
+			</Button>
+			<div>
+				<Button
+					variant="outlined"
+					id="basic-button"
+					aria-controls={open ? "basic-menu" : undefined}
+					aria-haspopup="true"
+					aria-expanded={open ? "true" : undefined}
+					onClick={handleClick}
+					endIcon={<ArrowDropDownIcon/>}>
+					<MoreHoriz />
+				</Button>
+				<Menu
+					id="basic-menu"
+					anchorEl={anchorEl}
+					open={open}
+					onClose={handleClose}
+					MenuListProps={{
+						"aria-labelledby": "basic-button",
+					}}
+				>
+					<MenuItem onClick={()=>{
+						handleClose();
+						setUpdateFileOpen(true);
+					}}> <ListItemIcon>
+							<Upload fontSize="small"/>
+						</ListItemIcon>
+						<ListItemText>Update File</ListItemText></MenuItem>
+					<MenuItem onClick={()=>{
+						handleClose();
+						setConfirmationOpen(true);
+					}}>
+						<ListItemIcon>
+							<DeleteIcon fontSize="small" />
+						</ListItemIcon>
+						<ListItemText>Delete</ListItemText>
+					</MenuItem>
+				</Menu>
+			</div>
+		</Stack>);
+};
+    
