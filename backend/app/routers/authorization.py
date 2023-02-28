edace336345891b6cc45c7d85ea2968a42732a1d
Max Burnette <mburnet2@illinois.edu>
2023-02-28 08:30:55 -0600

    fix response model

--- backend/app/routers/authorization.py
+++ backend/app/routers/authorization.py
@@ -60,7 +60,7 @@ async def get_dataset_role_owner(
     return {"dataset_id": dataset_id, "allow": allow}
 
 
-@router.get("/files/{file_id}/role", response_model=AuthorizationBase)
+@router.get("/files/{file_id}/role", response_model=AuthorizationFile)
 async def get_file_role(
     file_id: str,
     current_user=Depends(get_current_username),
