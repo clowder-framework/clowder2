74e6e7416f219871835359ff64f9c3f6c9eea952
Chen Wang <cwang138@illinois.edu>
2023-02-28 16:45:59 -0600

    add test database

---
+++ scripts/mongoviews/mongo-init-dev.js
@@ -0,0 +1,281 @@
+function init(databaseName) {
+	db = db.getSiblingDB(databaseName);
+
+	db.createCollection("authorization");
+	db.createCollection("datasets");
+	db.createCollection("feeds");
+	db.createCollection("file_versions");
+	db.createCollection("files");
+	db.createCollection("folders");
+	db.createCollection("listener_job_updates");
+	db.createCollection("listener_jobs");
+	db.createCollection("listeners");
+	db.createCollection("metadata");
+	db.createCollection("metadata.definitions");
+	db.createCollection("tokens");
+	db.createCollection("users");
+
+	db.getCollection("datasets").aggregate(
+		[
+			{
+				$lookup: {
+					"from": "authorization",
+					"localField": "_id",
+					"foreignField": "dataset_id",
+					"as": "auth"
+				}
+			}
+		],
+		{}
+	);
+
+	db.getCollection("files").aggregate(
+		[
+			{
+				$lookup: {
+					"from": "authorization",
+					"localField": "dataset_id",
+					"foreignField": "dataset_id",
+					"as": "auth"
+				}
+			}
+		],
+		{}
+	);
+
+	db.getCollection("folders").aggregate(
+		[
+			{
+				$lookup: {
+					"from": "authorization",
+					"localField": "dataset_id",
+					"foreignField": "dataset_id",
+					"as": "auth"
+				}
+			}
+		],
+		{}
+	);
+
+	db.getCollection("listener_job_updates").aggregate(
+		[
+			{
+				$lookup: // Equality Match
+					{
+						from: "listener_jobs",
+						localField: "job_id",
+						foreignField: "_id",
+						as: "listener_job_details"
+					}
+			},
+			{
+				$facet: {
+					"extraction_on_dataset": [
+						{
+							"$match": {
+								"listener_job_details.resource_ref.collection": {
+									"$eq": "dataset"
+								}
+							}
+						},
+						{
+							"$lookup": {
+								"from": "authorization",
+								"localField": "listener_job_details.resource_ref.resource_id",
+								"foreignField": "dataset_id",
+								"as": "auth"
+							}
+						}
+					],
+					"extraction_on_file": [
+						{
+							"$match": {
+								"listener_job_details.resource_ref.collection": {
+									"$eq": "file"
+								}
+							}
+						},
+						{
+							"$lookup": {
+								"from": "files",
+								"localField": "listener_job_details.resource_ref.resource_id",
+								"foreignField": "_id",
+								"as": "file_details"
+							}
+						},
+						{
+							"$lookup": {
+								"from": "authorization",
+								"localField": "file_details.dataset_id",
+								"foreignField": "dataset_id",
+								"as": "auth"
+							}
+						}
+					]
+				}
+			},
+			{
+				$project: {
+					"all": {
+						"$concatArrays": [
+							"$extraction_on_dataset",
+							"$extraction_on_file"
+						]
+					}
+				}
+			},
+			{
+				$unwind: "$all"
+			},
+			{
+				$replaceRoot: {
+					"newRoot": "$all"
+				}
+			}
+		],
+		{}
+	);
+
+	db.getCollection("listener_jobs").aggregate(
+		[
+			{
+				$facet: {
+					"extraction_on_dataset": [
+						{
+							"$match": {
+								"resource_ref.collection": {
+									"$eq": "dataset"
+								}
+							}
+						},
+						{
+							"$lookup": {
+								"from": "authorization",
+								"localField": "resource_ref.resource_id",
+								"foreignField": "dataset_id",
+								"as": "auth"
+							}
+						}
+					],
+					"extraction_on_file": [
+						{
+							"$match": {
+								"resource_ref.collection": {
+									"$eq": "file"
+								}
+							}
+						},
+						{
+							"$lookup": {
+								"from": "files",
+								"localField": "resource_ref.resource_id",
+								"foreignField": "_id",
+								"as": "file_details"
+							}
+						},
+						{
+							"$lookup": {
+								"from": "authorization",
+								"localField": "file_details.dataset_id",
+								"foreignField": "dataset_id",
+								"as": "auth"
+							}
+						}
+					]
+				}
+			},
+			{
+				$project: {
+					"all": {
+						"$concatArrays": [
+							"$extraction_on_dataset",
+							"$extraction_on_file"
+						]
+					}
+				}
+			},
+			{
+				$unwind: "$all"
+			},
+			{
+				$replaceRoot: {
+					"newRoot": "$all"
+				}
+			}
+		],
+		{}
+	);
+
+	db.getCollection("metadata").aggregate(
+		[
+			{
+				$facet: {
+					"metadata_on_dataset": [
+						{
+							"$match": {
+								"resource.collection": {
+									"$eq": "datasets"
+								}
+							}
+						},
+						{
+							"$lookup": {
+								"from": "authorization",
+								"localField": "resource.resource_id",
+								"foreignField": "dataset_id",
+								"as": "auth"
+							}
+						}
+					],
+					"metadata_on_file": [
+						{
+							"$match": {
+								"resource.collection": {
+									"$eq": "files"
+								}
+							}
+						},
+						{
+							"$lookup": {
+								"from": "files",
+								"localField": "resource.resource_id",
+								"foreignField": "_id",
+								"as": "file_details"
+							}
+						},
+						{
+							"$lookup": {
+								"from": "authorization",
+								"localField": "file_details.dataset_id",
+								"foreignField": "dataset_id",
+								"as": "auth"
+							}
+						}
+					]
+				}
+			},
+			{
+				$project: {
+					"all": {
+						"$concatArrays": [
+							"$metadata_on_dataset",
+							"$metadata_on_file"
+						]
+					}
+				}
+			},
+			{
+				$unwind: "$all"
+			},
+			{
+				$replaceRoot: {
+					"newRoot": "$all"
+				}
+			}
+		],
+		{}
+	);
+}
+
+init("clowder2");
+init("clowder-tests");
