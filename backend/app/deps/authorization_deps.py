9a72a645bc23ecdc16bafb6f82e4da9641048989
Chen Wang <cwang138@illinois.edu>
2023-03-14 16:28:41 -0500

    switch to role

--- backend/app/deps/authorization_deps.py
+++ backend/app/deps/authorization_deps.py
@@ -44,7 +44,7 @@ async def get_role_by_file(
                 ]
             }
         )
-        role = AuthorizationDB.from_mongo(authorization)
+        role = AuthorizationDB.from_mongo(authorization).role
         return role
 
     raise HTTPException(status_code=404, detail=f"File {file_id} not found")
