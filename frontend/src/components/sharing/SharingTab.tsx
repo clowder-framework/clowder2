69688cb667249e94f19d87cdf5550dfb7e5d351e
toddn <tcnichol@illinois.edu>
2023-03-17 16:41:16 -0500

    nothing seems to be retrieved

--- frontend/src/components/sharing/SharingTab.tsx
+++ frontend/src/components/sharing/SharingTab.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {RootState} from "../../types/data";
+import {fetchDatasetGroupsAndRoles, fetchDatasetUsersAndRoles} from "../../actions/authorization";
+import {useDispatch, useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
 
 
 export const SharingTab = (): JSX.Element => {
+
+	const {datasetId} = useParams<{ datasetId?: string }>();
+
+	const dispatch = useDispatch();
+
+	const getUsersAndRoles = (datasetId: string) => dispatch(fetchDatasetUsersAndRoles(datasetId));
+	const getGroupsAndRoles = (datasetId: string) => dispatch(fetchDatasetGroupsAndRoles(datasetId));
+	const datasetUsersAndRolesList = useSelector((state: RootState) => state.dataset.usersAndRoles);
+	const datasetGroupsAndRolesList = useSelector((state: RootState) => state.dataset.groupsAndRoles);
+
+
+	useEffect(() => {
+		getUsersAndRoles(datasetId);
+	}, []);
+
+	useEffect(() => {
+		getGroupsAndRoles(datasetId);
+	}, []);
+
+
+	console.log(datasetUsersAndRolesList);
+	console.log(datasetGroupsAndRolesList);
+	console.log('dataset group and user roles');
 
 
 	return (
