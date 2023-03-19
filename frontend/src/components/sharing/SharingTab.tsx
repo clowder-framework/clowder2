aedcc65eaf6d888f954fd5f165d863573aeca88f
toddn <tcnichol@illinois.edu>
2023-03-19 13:15:34 -0500

    didn't use class

--- frontend/src/components/sharing/SharingTab.tsx
+++ frontend/src/components/sharing/SharingTab.tsx
@@ -5,14 +5,6 @@ import {useDispatch, useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
 
 
-type GroupAndRole = {
-	updateMetadata: any,
-	deleteMetadata: any,
-	resourceType: string | undefined,
-	resourceId: string | undefined,
-	version: number | undefined,
-}
-
 export const SharingTab = (): JSX.Element => {
 
 	const {datasetId} = useParams<{ datasetId?: string }>();
