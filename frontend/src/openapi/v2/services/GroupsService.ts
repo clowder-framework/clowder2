45e1f261f952f37067adc1bbbbf500307d204e5f
Chen Wang <cwang138@illinois.edu>
2023-03-24 11:25:08 -0500

    add codgen testing if action succeed?

--- frontend/src/openapi/v2/services/GroupsService.ts
+++ frontend/src/openapi/v2/services/GroupsService.ts
@@ -10,34 +10,6 @@ import { request as __request } from '../core/request';
 export class GroupsService {
 
     /**
-     * Edit Group
-     * @param groupId
-     * @param userId
-     * @param requestBody
-     * @returns GroupOut Successful Response
-     * @throws ApiError
-     */
-    public static editGroupApiV2GroupsPut(
-        groupId: string,
-        userId: string,
-        requestBody: GroupBase,
-    ): CancelablePromise<GroupOut> {
-        return __request({
-            method: 'PUT',
-            path: `/api/v2/groups`,
-            query: {
-                'group_id': groupId,
-                'user_id': userId,
-            },
-            body: requestBody,
-            mediaType: 'application/json',
-            errors: {
-                422: `Validation Error`,
-            },
-        });
-    }
-
-    /**
      * Save Group
      * @param requestBody
      * @returns GroupOut Successful Response
@@ -69,6 +41,33 @@ export class GroupsService {
         return __request({
             method: 'GET',
             path: `/api/v2/groups/${groupId}`,
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+
+    /**
+     * Edit Group
+     * @param groupId
+     * @param userId
+     * @param requestBody
+     * @returns GroupOut Successful Response
+     * @throws ApiError
+     */
+    public static editGroupApiV2GroupsGroupIdPut(
+        groupId: string,
+        userId: string,
+        requestBody: GroupBase,
+    ): CancelablePromise<GroupOut> {
+        return __request({
+            method: 'PUT',
+            path: `/api/v2/groups/${groupId}`,
+            query: {
+                'user_id': userId,
+            },
+            body: requestBody,
+            mediaType: 'application/json',
             errors: {
                 422: `Validation Error`,
             },
@@ -113,6 +112,7 @@ export class GroupsService {
 
     /**
      * Add Member
+     * Add a new user to a group.
      * @param groupId
      * @param username
      * @returns any Successful Response
@@ -133,6 +133,7 @@ export class GroupsService {
 
     /**
      * Remove Member
+     * Remove a user from a group.
      * @param groupId
      * @param username
      * @returns any Successful Response
