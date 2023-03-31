df16b0aeb7a709eceef54dc33faa31a3fc6957b7
Aruna Parameswaran <aruna.parame@gmail.com>
2023-03-31 07:22:47 -0500

    Re-executed eslint

--- frontend/src/components/datasets/ShareDatasetModal.tsx
+++ frontend/src/components/datasets/ShareDatasetModal.tsx
@@ -3,7 +3,7 @@ import { Alert, Autocomplete, Button, Collapse, Container, Dialog, DialogActions
 import {useParams} from "react-router-dom";
 import { setDatasetUserRole } from "../../actions/dataset";
 import { useDispatch } from "react-redux";
-import CloseIcon from '@mui/icons-material/Close'
+import CloseIcon from "@mui/icons-material/Close";
 
 
 type ShareDatasetModalProps = {
@@ -13,101 +13,101 @@ type ShareDatasetModalProps = {
 }
 
 export default function ShareDatasetModal(props: ShareDatasetModalProps) {
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const { open, handleClose, datasetName } = props;
+	const { open, handleClose, datasetName } = props;
 	const {datasetId} = useParams<{ datasetId?: string }>();
-    const [email, setEmail] = useState("")
-    const [role, setRole] = useState("viewer")
-    const [showSuccessAlert, setShowSuccessAlert] = useState(false)
+	const [email, setEmail] = useState("");
+	const [role, setRole] = useState("viewer");
+	const [showSuccessAlert, setShowSuccessAlert] = useState(false);
 
 	const setUserRole = (datasetId: string, username: string, role: string) => dispatch(setDatasetUserRole(datasetId, username, role));
 
-    // component did mount
+	// component did mount
 	useEffect(() => {
 		// listUsers();
 	}, []);
     
-    const onShare = () => {
-        setUserRole(datasetId, email, role);
-        setEmail("");
-        setRole("viewer")
-        setShowSuccessAlert(true);
-    }
+	const onShare = () => {
+		setUserRole(datasetId, email, role);
+		setEmail("");
+		setRole("viewer");
+		setShowSuccessAlert(true);
+	};
 
-    return (
-        <Container>
-            <Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth="md"
-                sx={{
-                    '.MuiPaper-root': {
-                        padding: "2em",
-                    },
-                }}>
-                <DialogTitle>Share dataset &apos;{datasetName}&apos;</DialogTitle>
-                <Divider />
-                <DialogContent>
-                    <Typography>Invite people to collaborate</Typography>
-                    <div style={{
-                        display: 'flex',
-                        alignItems: 'center'
-                    }}>
-                        <Autocomplete
-                            id="email-auto-complete"
-                            freeSolo
-                            autoHighlight
-                            inputValue={email}
-                            onInputChange={(event, value) => {
-                                setEmail(value)
-                            }}
-                            options={["abc@xyz.com"]}
-                            renderInput={(params) => <TextField {...params} sx={{ mt: 1, mr: 1, "alignItems": "right", "width": "450px" }} required label="Enter email address" />}
-                        /> as
-                        <FormControl variant="outlined" sx={{ m: 1, minWidth: 120 }}>
-                            <InputLabel id="demo-simple-select-label">Status</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={role}
-                                defaultValue={"viewer"}
-                                label="Status"
-                                onChange={(event, value) => {
-                                    setRole(event.target.value)
-                                }}
-                            >
-                                <MenuItem value="owner">Owner</MenuItem>
-                                <MenuItem value="editor">Editor</MenuItem>
-                                <MenuItem value="uploader">Uploader</MenuItem>
-                                <MenuItem value="viewer">Viewer</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </div>
-                    <Button variant="contained" sx={{ marginTop: 1 }} onClick={onShare} disabled={(email.length > 0) ? false : true}>Share</Button>
-                    <Collapse in={showSuccessAlert}>
-                        <br/>
-                        <Alert
-                            severity="success"
-                            action={
-                                <IconButton
-                                aria-label="close"
-                                color="inherit"
-                                size="small"
-                                onClick={() => {
-                                    setShowSuccessAlert(false);
-                                }}
-                                >
-                                    <CloseIcon fontSize="inherit" />
-                                </IconButton>
-                            }
-                            sx={{ mb: 2 }}
-                        >
+	return (
+		<Container>
+			<Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth="md"
+				sx={{
+					".MuiPaper-root": {
+						padding: "2em",
+					},
+				}}>
+				<DialogTitle>Share dataset &apos;{datasetName}&apos;</DialogTitle>
+				<Divider />
+				<DialogContent>
+					<Typography>Invite people to collaborate</Typography>
+					<div style={{
+						display: "flex",
+						alignItems: "center"
+					}}>
+						<Autocomplete
+							id="email-auto-complete"
+							freeSolo
+							autoHighlight
+							inputValue={email}
+							onInputChange={(event, value) => {
+								setEmail(value);
+							}}
+							options={["abc@xyz.com"]}
+							renderInput={(params) => <TextField {...params} sx={{ mt: 1, mr: 1, "alignItems": "right", "width": "450px" }} required label="Enter email address" />}
+						/> as
+						<FormControl variant="outlined" sx={{ m: 1, minWidth: 120 }}>
+							<InputLabel id="demo-simple-select-label">Status</InputLabel>
+							<Select
+								labelId="demo-simple-select-label"
+								id="demo-simple-select"
+								value={role}
+								defaultValue={"viewer"}
+								label="Status"
+								onChange={(event, value) => {
+									setRole(event.target.value);
+								}}
+							>
+								<MenuItem value="owner">Owner</MenuItem>
+								<MenuItem value="editor">Editor</MenuItem>
+								<MenuItem value="uploader">Uploader</MenuItem>
+								<MenuItem value="viewer">Viewer</MenuItem>
+							</Select>
+						</FormControl>
+					</div>
+					<Button variant="contained" sx={{ marginTop: 1 }} onClick={onShare} disabled={(email.length > 0) ? false : true}>Share</Button>
+					<Collapse in={showSuccessAlert}>
+						<br/>
+						<Alert
+							severity="success"
+							action={
+								<IconButton
+									aria-label="close"
+									color="inherit"
+									size="small"
+									onClick={() => {
+										setShowSuccessAlert(false);
+									}}
+								>
+									<CloseIcon fontSize="inherit" />
+								</IconButton>
+							}
+							sx={{ mb: 2 }}
+						>
                         Successfully added role!
-                        </Alert>
-                    </Collapse>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Close</Button>
-                </DialogActions>
-            </Dialog>
-        </Container>
-    );
+						</Alert>
+					</Collapse>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Close</Button>
+				</DialogActions>
+			</Dialog>
+		</Container>
+	);
 }
--- frontend/src/components/datasets/ShareGroupDatasetModal.tsx
+++ frontend/src/components/datasets/ShareGroupDatasetModal.tsx
@@ -6,7 +6,7 @@ import {fetchGroups} from "../../actions/group";
 import {RootState} from "../../types/data";
 import {setDatasetGroupRole} from "../../actions/dataset";
 import {useParams} from "react-router-dom";
-import CloseIcon from '@mui/icons-material/Close'
+import CloseIcon from "@mui/icons-material/Close";
 
 type ShareGroupDatasetModalProps = {
     open: boolean,
@@ -19,7 +19,7 @@ export default function ShareGroupDatasetModal(props: ShareGroupDatasetModalProp
 	const {datasetId} = useParams<{ datasetId?: string }>();
     	const [role, setRole] = useState("viewer");
 	const [group, setGroup] = useState("");
-    	const [showSuccessAlert, setShowSuccessAlert] = useState(false)
+    	const [showSuccessAlert, setShowSuccessAlert] = useState(false);
 	const dispatch = useDispatch();
 	const listGroups = () => dispatch(fetchGroups(0, 21));
 	const groups = useSelector((state: RootState) => state.group.groups);
@@ -31,112 +31,112 @@ export default function ShareGroupDatasetModal(props: ShareGroupDatasetModalProp
 		listGroups();
 	}, []);
 
-    const onShare = () => {
+	const onShare = () => {
     	console.log(group, datasetId,role);
     	setGroupRole(datasetId, group, role);
-        setGroup("")
-        setRole("viewer")
-        setShowSuccessAlert(true);
-    }
+		setGroup("");
+		setRole("viewer");
+		setShowSuccessAlert(true);
+	};
 
 
 
 	const options = Array();
     	groups.map((group) => {
-    	let group_option = {value:group.id, label:group.name}
+    	const group_option = {value:group.id, label:group.name};
 		options.push(group_option);
     	});
-	console.log('group optioins are', options);
-	console.log('it is of type', typeof(options));
+	console.log("group optioins are", options);
+	console.log("it is of type", typeof(options));
 
 	return (
-        <Container>
-            <Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth="md"
-                sx={{
-                    '.MuiPaper-root': {
-                        padding: "2em",
-                    },
-                }}>
-                <DialogTitle>Share dataset &apos;{datasetName}&apos;</DialogTitle>
-                <Divider />
-                <DialogContent>
-                    <Typography>Invite groups to collaborate</Typography>
-                    <div style={{
-                        display: 'flex',
-                        alignItems: 'center'
-                    }}>
-			<FormControl variant="outlined" sx={{ m: 1, minWidth: 120 }}>
-				<InputLabel id="demo-simple-select-label">Group</InputLabel>
-				<Select
+		<Container>
+			<Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth="md"
+				sx={{
+					".MuiPaper-root": {
+						padding: "2em",
+					},
+				}}>
+				<DialogTitle>Share dataset &apos;{datasetName}&apos;</DialogTitle>
+				<Divider />
+				<DialogContent>
+					<Typography>Invite groups to collaborate</Typography>
+					<div style={{
+						display: "flex",
+						alignItems: "center"
+					}}>
+						<FormControl variant="outlined" sx={{ m: 1, minWidth: 120 }}>
+							<InputLabel id="demo-simple-select-label">Group</InputLabel>
+							<Select
                                 	labelId="demo-simple-select-label"
                                 	id="demo-simple-select"
-					value={group}
-					// defaultValue={"641214ebfe6405bc949730c6"}
-					label="Group"
+								value={group}
+								// defaultValue={"641214ebfe6405bc949730c6"}
+								label="Group"
                                 	onChange={(event, value) => {
                                 		console.log(event, value);
-                                		console.log('chose a new group');
+                                		console.log("chose a new group");
                                 		console.log(event.target.value);
                                     		setGroup(event.target.value);
                                 	}}
                             	>
-					{options.map((group) => (
+								{options.map((group) => (
             					<MenuItem
-							key={group.label}
-							value={group.value}
-						>
-							{group.label}
+										key={group.label}
+										value={group.value}
+									>
+										{group.label}
             					</MenuItem>
-					))}
+								))}
                             	</Select>
-			</FormControl>
-                        <FormControl variant="outlined" sx={{ m: 1, minWidth: 120 }}>
-                            <InputLabel id="demo-simple-select-label">Status</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={role}
-                                defaultValue={"viewer"}
-                                label="Status"
-                                onChange={(event, value) => {
+						</FormControl>
+						<FormControl variant="outlined" sx={{ m: 1, minWidth: 120 }}>
+							<InputLabel id="demo-simple-select-label">Status</InputLabel>
+							<Select
+								labelId="demo-simple-select-label"
+								id="demo-simple-select"
+								value={role}
+								defaultValue={"viewer"}
+								label="Status"
+								onChange={(event, value) => {
                                 	console.log(event, value);
-                                    setRole(event.target.value)
-                                }}
-                            >
-                                <MenuItem value="owner">Owner</MenuItem>
-                                <MenuItem value="editor">Editor</MenuItem>
-                                <MenuItem value="uploader">Uploader</MenuItem>
-                                <MenuItem value="viewer">Viewer</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </div>
-                    <Button variant="contained" sx={{ marginTop: 1 }} onClick={onShare} disabled={(group.length > 0) ? false : true}>Share</Button>
-                    <Collapse in={showSuccessAlert}>
-                        <br/>
-                        <Alert
-                            severity="success"
-                            action={
-                                <IconButton
-                                aria-label="close"
-                                color="inherit"
-                                size="small"
-                                onClick={() => {
-                                    setShowSuccessAlert(false);
-                                }}
-                                >
-                                    <CloseIcon fontSize="inherit" />
-                                </IconButton>
-                            }
-                            sx={{ mb: 2 }}
-                        >
+									setRole(event.target.value);
+								}}
+							>
+								<MenuItem value="owner">Owner</MenuItem>
+								<MenuItem value="editor">Editor</MenuItem>
+								<MenuItem value="uploader">Uploader</MenuItem>
+								<MenuItem value="viewer">Viewer</MenuItem>
+							</Select>
+						</FormControl>
+					</div>
+					<Button variant="contained" sx={{ marginTop: 1 }} onClick={onShare} disabled={(group.length > 0) ? false : true}>Share</Button>
+					<Collapse in={showSuccessAlert}>
+						<br/>
+						<Alert
+							severity="success"
+							action={
+								<IconButton
+									aria-label="close"
+									color="inherit"
+									size="small"
+									onClick={() => {
+										setShowSuccessAlert(false);
+									}}
+								>
+									<CloseIcon fontSize="inherit" />
+								</IconButton>
+							}
+							sx={{ mb: 2 }}
+						>
                         Successfully added role!
-                        </Alert>
-                    </Collapse>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Close</Button>
-                </DialogActions>
-            </Dialog>
-        </Container>
-    );
+						</Alert>
+					</Collapse>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Close</Button>
+				</DialogActions>
+			</Dialog>
+		</Container>
+	);
 }
