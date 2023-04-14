db2619fa3d07be5b67a9555e91ecded7d96cb972
Aruna Parameswaran <aruna.parame@gmail.com>
2023-04-13 23:56:12 -0500

    Revert "Metadata on new file version not updated"
    This reverts commit d1ca0ac3606faea67ecf7b7d4cb96c3c56ab715b.

--- frontend/src/components/files/UpdateFile.tsx
+++ frontend/src/components/files/UpdateFile.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 
 import {Box, Button, Container} from "@mui/material";
 
@@ -8,11 +8,8 @@ import Form from "@rjsf/material-ui";
 
 import fileSchema from "../../schema/fileSchema.json";
 import {FormProps} from "@rjsf/core";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {fetchFileVersions, fileUpdated} from "../../actions/file";
-import { fetchFileMetadata, postFileMetadata } from "../../actions/metadata";
-import { RootState } from "../../types/data";
-import { MetadataIn } from "../../openapi/v2";
 
 
 type UpdateFileProps ={
@@ -24,37 +21,14 @@ export const UpdateFile: React.FC<UpdateFileProps> = (props: UpdateFileProps) =>
 	const dispatch = useDispatch();
 	const updateFile = async (formData: FormData, fileId: string|undefined) => dispatch(fileUpdated(formData, fileId));
 	const listFileVersions = (fileId: string | undefined) => dispatch(fetchFileVersions(fileId));
-	const listFileMetadata = async (fileId: string | undefined) => dispatch(fetchFileMetadata(fileId));
-	const createFileMetadata = (fileId: string|undefined, metadata:MetadataIn) => dispatch(postFileMetadata(fileId, metadata));
-	const fileMetadataList = useSelector((state: RootState) => state.metadata.fileMetadataList);
 
 	const {fileId, setOpen,} = props;
 
 	const [loading, setLoading] = useState<boolean>(false);
 
-	useEffect(() => {
-		listFileMetadata(fileId);
-	}, []);
-
 	const onSave = async (formData:FormData) => {
 		setLoading(true);
 		await updateFile(formData, fileId);
-
-		// Update metadata entry for new version of file in db
-		fileMetadataList.forEach((item, idx) => {
-			if (item.definition !== null) {
-				const metadata: MetadataIn = {
-					context: item.context,
-					context_url: item.context_url,
-					content: item.content,
-					definition: item.definition,
-					file_version: item.resource.version + 1
-				};
-
-				createFileMetadata(fileId, metadata);
-			}
-		});
-
 		setLoading(false);
 		setOpen(false);
 		listFileVersions(fileId);
