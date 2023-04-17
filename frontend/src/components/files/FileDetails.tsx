89ee2ccdfaa41d1954ee422bb7746776e6ce1c1b
Luigi Marini <lmarini@illinois.edu>
2023-04-17 10:31:03 -0500

    Downloads count is now in the details list to simplify UI on file and dataset pages. Used to have it's own statistics block but it was the only entry.

--- frontend/src/components/files/FileDetails.tsx
+++ frontend/src/components/files/FileDetails.tsx
@@ -1,40 +1,42 @@
 import React from "react";
-import {Box, Typography} from "@mui/material";
-import {parseDate} from "../../utils/common";
-import {FileOut} from "../../openapi/v2";
+import { Box, Typography } from "@mui/material";
+import { parseDate } from "../../utils/common";
+import { FileOut } from "../../openapi/v2";
 import prettyBytes from "pretty-bytes";
-import {StackedList} from "../util/StackedList";
-
+import { StackedList } from "../util/StackedList";
 
 type FileAboutProps = {
-	fileSummary: FileOut
-}
+	fileSummary: FileOut;
+};
 
 export function FileDetails(props: FileAboutProps) {
-	const {id, created, name, creator, version_id, bytes, content_type, views, downloads} = props.fileSummary;
+	const {
+		id,
+		created,
+		name,
+		creator,
+		version_id,
+		bytes,
+		content_type,
+		views,
+		downloads,
+	} = props.fileSummary;
 
 	const details = new Map<string, string>();
 	details.set("Size", prettyBytes(bytes));
 	details.set("Content type", content_type.content_type);
 	details.set("Updated on", parseDate(created));
 	details.set("Uploaded as", name);
-	details.set("Uploaded by", creator.first_name + " " + creator.last_name);
+	details.set("Uploaded by", `${creator.first_name} ${creator.last_name}`);
 	details.set("File id", id);
-
-	const stats = new Map<string, string>();
-	stats.set("Downloads", downloads);
+	details.set("Downloads", downloads);
 
 	return (
-		<Box>
-			<Box sx={{mt: 5}}>
-				<Typography variant="h5" gutterBottom>Details</Typography>
-				<StackedList keyValues={details}/>
-			</Box>
-			<Box sx={{mt: 5}}>
-				<Typography variant="h5" gutterBottom>Statistics</Typography>
-				<StackedList keyValues={stats}/>
-			</Box>
+		<Box sx={{ mt: 5 }}>
+			<Typography variant="h5" gutterBottom>
+				Details
+			</Typography>
+			<StackedList keyValues={details} />
 		</Box>
-	)
+	);
 }
-
