6c1ee593ddc10f14beca366337eb9f245830893b
Luigi Marini <lmarini@users.noreply.github.com>
2023-04-18 14:28:49 -0500

    Minor UI updates (#436)
    * Downloads count is now in the details list to simplify UI on file and dataset pages.
    Used to have it's own statistics block but it was the only entry.
    
    * Capitalize text in extractor filters.
    
    * Moved permission rolechip below title and made it smaller.
    Switched to just use the visibility icon for consistency and clarity.
    Only capitalize first character.
    
    * Removed breadcrumb from dataset page. Wasn't adding much with just the Explore link and repeated dataset tile.
    I think this was already changed in the past, but maybe lost in a merge conflict.
    Dropped "Explore" from file breadcrumb.
    
    * Center Add Metadata button on file and dataset pages.
    
    * File tabs are now consistent with dataset tabs.
    Moved tab css to central file Styles.tsx.
    
    * Added play button to launch extractor to list of extractors.

--- frontend/src/components/datasets/DatasetDetails.tsx
+++ frontend/src/components/datasets/DatasetDetails.tsx
@@ -1,40 +1,31 @@
 import React from "react";
-import {Box, Typography} from "@mui/material";
-import {parseDate} from "../../utils/common";
-import prettyBytes from "pretty-bytes";
-import {StackedList} from "../util/StackedList";
-import {Dataset} from "../../types/data";
-
+import { Box, Typography } from "@mui/material";
+import { parseDate } from "../../utils/common";
+import { StackedList } from "../util/StackedList";
+import { Dataset } from "../../types/data";
 
 type DatasetAboutProps = {
-	details: Dataset
-
-}
+	details: Dataset;
+};
 
 export function DatasetDetails(props: DatasetAboutProps) {
-	const {id, created, modified, author, status, views, downloads} = props.details;
+	const { id, created, modified, author, status, views, downloads } =
+		props.details;
 
 	const details = new Map<string, string>();
-	details.set("Owner", author.first_name + " " + author.last_name);
+	details.set("Owner", `${author.first_name} ${author.last_name}`);
 	details.set("Created on", parseDate(created));
 	details.set("Updated on", parseDate(modified));
 	details.set("Status", status);
 	details.set("Dataset id", id);
-
-	const stats = new Map<string, string>();
-	stats.set("Downloads", downloads);
+	details.set("Downloads", downloads);
 
 	return (
-		<Box>
-			<Box sx={{mt: 5}}>
-				<Typography variant="h5" gutterBottom>Details</Typography>
-				<StackedList keyValues={details}/>
-			</Box>
-			<Box sx={{mt: 5}}>
-				<Typography variant="h5" gutterBottom>Statistics</Typography>
-				<StackedList keyValues={stats}/>
-			</Box>
+		<Box sx={{ mt: 5 }}>
+			<Typography variant="h5" gutterBottom>
+				Details
+			</Typography>
+			<StackedList keyValues={details} />
 		</Box>
-	)
+	);
 }
-
