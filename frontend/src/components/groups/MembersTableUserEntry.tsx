aec657d93b6a1be43dfc485019e2c560ececb8ae
toddn <tcnichol@illinois.edu>
2023-04-18 14:28:32 -0500

    using more conditionals to reduce duplicate code

--- frontend/src/components/groups/MembersTableUserEntry.tsx
+++ frontend/src/components/groups/MembersTableUserEntry.tsx
@@ -62,100 +62,86 @@ export function MembersTableUserEntry(props: MembersTableUserEntryProps) {
 		setEditRoleOn(false);
 	};
 
-	if (member.user.email == creatorEmail) {
-		return (
-			<TableRow
-				key={member.user.id}
-				sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-			>
-				<TableCell component="th" scope="row" key={`${member.user.id}-icon`}>
-					{
-						member && member.user && member.user.email ?
-							<Gravatar email={member.user.email} rating="g"
-								  style={{width: "32px", height: "32px", borderRadius: "50%", verticalAlign: "middle"}}/>
-							:
-							<PersonIcon sx={iconStyle}/>
-					}
-					<Button >{member.user.first_name} {member.user.last_name}</Button>
-				</TableCell>
-				<TableCell align="right">{member.user.email}</TableCell>
+	return (
+		<TableRow
+			key={member.user.id}
+			sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+		>
+			<TableCell component="th" scope="row" key={`${member.user.id}-icon`}>
+				{
+					member && member.user && member.user.email ?
+						<Gravatar email={member.user.email} rating="g"
+							  style={{width: "32px", height: "32px", borderRadius: "50%", verticalAlign: "middle"}}/>
+						:
+						<PersonIcon sx={iconStyle}/>
+				}
+				<Button >{member.user.first_name} {member.user.last_name}</Button>
+			</TableCell>
+			<TableCell align="right">{member.user.email}</TableCell>
+			{member.user.email == creatorEmail &&
 				<TableCell align="right">{"Owner"}
 				</TableCell>
+			}
+			{member.user.email != creatorEmail &&
 				<TableCell align="right">
-				</TableCell>
-			</TableRow>
-		)
-	} else {
-		return (
-			<TableRow
-				key={member.user.id}
-				sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-			>
-				<TableCell component="th" scope="row" key={`${member.user.id}-icon`}>
-					{
-						member && member.user && member.user.email ?
-							<Gravatar email={member.user.email} rating="g"
-								  style={{width: "32px", height: "32px", borderRadius: "50%", verticalAlign: "middle"}}/>
-							:
-							<PersonIcon sx={iconStyle}/>
-					}
-					<Button >{member.user.first_name} {member.user.last_name}</Button>
-				</TableCell>
-				<TableCell align="right">{member.user.email}</TableCell>
-				<TableCell align="right">
+				{
+					editRoleOn ?
+						<FormControl size="small">
+							  <InputLabel id="demo-simple-select-label">Role</InputLabel>
+							  <Select
+								labelId="role"
+								id="role"
+								value={selectedRole}
+								label="Role"
+								onChange={handleRoleSelection}
+							  >
+								<MenuItem value={"editor"}>Editor</MenuItem>
+								<MenuItem value={"member"}>Member</MenuItem>
+							  </Select>
+							</FormControl>
+						:
+						member.editor !== undefined && member.editor ?
+							"Editor" : "Member"
+				}
+				{/*only owner or editor are allowed to modify roles of the member*/}
+				<AuthWrapper currRole={role} allowedRoles={["owner", "editor"]}>
 					{
 						editRoleOn ?
-							<FormControl size="small">
-								  <InputLabel id="demo-simple-select-label">Role</InputLabel>
-								  <Select
-									labelId="role"
-									id="role"
-									value={selectedRole}
-									label="Role"
-									onChange={handleRoleSelection}
-								  >
-									<MenuItem value={"editor"}>Editor</MenuItem>
-									<MenuItem value={"member"}>Member</MenuItem>
-								  </Select>
-								</FormControl>
-							:
-							member.editor !== undefined && member.editor ?
-								"Editor" : "Member"
-					}
-					{/*only owner or editor are allowed to modify roles of the member*/}
-					<AuthWrapper currRole={role} allowedRoles={["owner", "editor"]}>
-						{
-							editRoleOn ?
-								<ButtonGroup variant="text">
-									<IconButton type="button" sx={{p: "10px"}} onClick={handleRoleSave}>
-										<CheckIcon sx={iconStyle}/>
-									</IconButton>
-									<IconButton type="button" sx={{p: "10px"}} onClick={handleRoleCancel}>
-										<CloseIcon sx={iconStyle}/>
-									</IconButton>
-								</ButtonGroup>
-								:
-								<IconButton type="button" sx={{p: "10px"}} aria-label="edit" onClick={()=>{
-									setEditRoleOn(true);
-								}}>
-									<EditIcon sx={iconStyle} />
+							<ButtonGroup variant="text">
+								<IconButton type="button" sx={{p: "10px"}} onClick={handleRoleSave}>
+									<CheckIcon sx={iconStyle}/>
 								</IconButton>
-						}
-					</AuthWrapper>
-				</TableCell>
+								<IconButton type="button" sx={{p: "10px"}} onClick={handleRoleCancel}>
+									<CloseIcon sx={iconStyle}/>
+								</IconButton>
+							</ButtonGroup>
+							:
+							<IconButton type="button" sx={{p: "10px"}} aria-label="edit" onClick={()=>{
+								setEditRoleOn(true);
+							}}>
+								<EditIcon sx={iconStyle} />
+							</IconButton>
+					}
+				</AuthWrapper>
+			</TableCell>
+			}
+			{member.user.email == creatorEmail &&
 				<TableCell align="right">
-					{/*only owner or editor are allowed to delete*/}
-					<AuthWrapper currRole={role} allowedRoles={["owner", "editor"]}>
-						<IconButton type="button" sx={{p: "10px"}} aria-label="delete" onClick={()=>{
-							setSelectMemberUsername(member.user.email)
-							setDeleteMemberConfirmOpen(true);
-						}}>
-							<DeleteIcon sx={iconStyle}/>
-						</IconButton>
-					</AuthWrapper>
 				</TableCell>
-			</TableRow>
-		)
-	}
-
+			}
+			{member.user.email != creatorEmail &&
+				<TableCell align="right">
+				{/*only owner or editor are allowed to delete*/}
+				<AuthWrapper currRole={role} allowedRoles={["owner", "editor"]}>
+					<IconButton type="button" sx={{p: "10px"}} aria-label="delete" onClick={()=>{
+						setSelectMemberUsername(member.user.email)
+						setDeleteMemberConfirmOpen(true);
+					}}>
+						<DeleteIcon sx={iconStyle}/>
+					</IconButton>
+				</AuthWrapper>
+			</TableCell>
+			}
+		</TableRow>
+	)
 }
