11654f8e4ec412e79e845fd2dbb1111eda69e87a
Aruna Parameswaran <aruna.parame@gmail.com>
2023-04-18 20:20:23 -0500

    Fixed the props for search component
    * Toggling Advanced switch caused page to break
    
    * Debug logs showed that the properties were not valid, updated code to reflect the same

--- frontend/src/components/search/SearchResult.tsx
+++ frontend/src/components/search/SearchResult.tsx
@@ -23,29 +23,29 @@ export function SearchResult(props) {
 						{
 							item._index === "dataset" ?
 								<MuiLink component={Link} to={`/datasets/${item._id}`}
-										 sx={{fontWeight: "bold", fontSize: "18px"}}>
-									{parse(item.name)}
+									sx={{fontWeight: "bold", fontSize: "18px"}}>
+									{item.name}
 								</MuiLink>
 								:
 								<MuiLink component={Link} to={`/files/${item._id}?dataset=${item.dataset_id}`}
-										 sx={{fontWeight: "bold", fontSize: "18px"}}>
-									{parse(item.name)}
+									sx={{fontWeight: "bold", fontSize: "18px"}}>
+									{item.name}
 								</MuiLink>
 						}
 						<Typography variant="body2" color={theme.palette.secondary.light}>
 							{
 								item._index === "dataset" ?
-									`Created by ${parse(item.author)} at ${parseDate(item.created)}`
+									`Created by ${item.author} at ${parseDate(item.created)}`
 									:
-									`Created by ${parse(item.creator)} at ${parseDate(item.created)}`
+									`Created by ${item.creator} at ${parseDate(item.created)}`
 							}
 						</Typography>
 						<Typography variant="body2" color={theme.palette.secondary.dark}>
-							{item._index === "dataset" ? parse(item.description) : `${item.content_type.content_type} | ${item.bytes} bytes`}
+							{item._index === "dataset" ? item.description : `${item.content_type} | ${item.bytes} bytes`}
 						</Typography>
 					</Box>
 				</ListItem>
-				))}
+			))}
 		</List>
 	);
 
