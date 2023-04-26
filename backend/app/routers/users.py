bc73f107c3fd07e1630a4ee0582ac75f851b9f05
toddn <tcnichol@illinois.edu>
2023-04-26 12:57:36 -0500

    new route for user profile profile page includes layout

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -103,6 +103,13 @@ async def get_users(
         users.append(UserOut(**doc))
     return users
 
+@router.get("/profile", response_model=UserOut)
+async def get_profile(
+    username=Depends(get_current_username), db: MongoClient = Depends(dependencies.get_db)
+):
+    if (user := await db["users"].find_one({"email": username})) is not None:
+        return UserOut.from_mongo(user)
+    raise HTTPException(status_code=404, detail=f"User {username} not found")
 
 @router.get("/{user_id}", response_model=UserOut)
 async def get_user(user_id: str, db: MongoClient = Depends(dependencies.get_db)):
--- frontend/src/components/users/Profile.tsx
+++ frontend/src/components/users/Profile.tsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
+import { Box, Button, Grid, Stack, Tab, Tabs, Typography } from "@mui/material";
+import { useParams, useSearchParams } from "react-router-dom";
+import { RootState } from "../../types/data";
+import {useSelector} from "react-redux";
+import Layout from "../Layout";
 
 export const Profile = (): JSX.Element => {
+	const user = useSelector((state: RootState) => state.user);
+	console.log('user is');
+	console.log(user);
 	return (
-		<>nothing yet</>
+		<Layout>
+			<>profile would go here</>
+		</Layout>
 	)
 
 }
