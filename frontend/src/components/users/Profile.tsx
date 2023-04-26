e659de9b010d6d733a5c576a02c6e7fb25fa6b3a
toddn <tcnichol@illinois.edu>
2023-04-26 14:12:20 -0500

    page has fake data

--- frontend/src/components/users/Profile.tsx
+++ frontend/src/components/users/Profile.tsx
@@ -4,7 +4,13 @@ import { useParams, useSearchParams } from "react-router-dom";
 import { RootState } from "../../types/data";
 import { useDispatch, useSelector } from "react-redux";
 import Layout from "../Layout";
-import {fetchFolderPath} from "../../actions/folder";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import {fetchUserProfile} from "../../actions/user";
 
 export const Profile = (): JSX.Element => {
@@ -22,10 +28,29 @@ export const Profile = (): JSX.Element => {
 
 	console.log('profile is', profile);
 	return (
-
-		<Layout>
-			<>profile would go here</>
-		</Layout>
+			<Layout>
+				<TableContainer component={Paper}>
+					<Table sx={{minWidth: 650}} aria-label="simple table">
+						<TableHead>
+							<TableRow>
+								<TableCell>Name</TableCell>
+								<TableCell align="right">Email</TableCell>
+								<TableCell align="right">Admin</TableCell>
+								<TableCell align="right"></TableCell>
+							</TableRow>
+						</TableHead>
+							<TableBody>
+								<TableRow
+									sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+		>
+									<TableCell>{"name"}</TableCell>
+									<TableCell align="right">{"a@a.com"}</TableCell>
+									<TableCell align="right">{"false"}</TableCell>
+								</TableRow>
+							</TableBody>
+					</Table>
+				</TableContainer>
+			</Layout>
 	)
 
 }
