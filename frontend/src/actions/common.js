589be284bda7be34f6bda96ffc0b059edd9db02f
Aruna Parameswaran <aruna.parame@gmail.com>
2023-05-01 10:22:31 -0500

    Fixed bug where error detail might be empty
    * Added null check to the detail string to prevent errors when detail is not set

--- frontend/src/actions/common.js
+++ frontend/src/actions/common.js
@@ -64,7 +64,7 @@ export function handleErrors(reason, originalFunc){
 		return (dispatch) => {
 			dispatch({
 				type: NOT_AUTHORIZED,
-				reason: reason.body.detail,
+				reason: reason.body.detail !== undefined? reason.body.detail : "Forbidden",
 				stack: reason.stack ? reason.stack : "",
 				receivedAt: Date.now()
 			});
@@ -73,7 +73,7 @@ export function handleErrors(reason, originalFunc){
 		return (dispatch) => {
 			dispatch({
 				type: NOT_FOUND,
-				reason: reason.body.detail,
+				reason: reason.body.detail !== undefined? reason.body.detail : "Not Found",
 				stack: reason.stack ? reason.stack : "",
 				receivedAt: Date.now()
 			});
