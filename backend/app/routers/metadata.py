4f9a735a7eae82c935979d66b4ff84ad4f692b96
Chen Wang <cwang138@illinois.edu>
2023-05-04 17:17:43 -0500

    refactoring the metadata route

--- backend/app/routers/metadata.py
+++ backend/app/routers/metadata.py
@@ -1,5 +1,3 @@
-import io
-from datetime import datetime
 from typing import Optional, List
 
 from fastapi import (
@@ -10,59 +8,55 @@ from fastapi import (
 from pymongo import MongoClient
 
 from app import dependencies
-from app.deps.authorization_deps import Authorization, MetadataAuthorization
-from app.keycloak_auth import get_user, get_current_user
-from app.models.pyobjectid import PyObjectId
+from app.deps.authorization_deps import MetadataAuthorization
+from app.keycloak_auth import get_current_user
 from app.models.metadata import (
     MetadataDefinitionIn,
     MetadataDefinitionDB,
     MetadataDefinitionOut,
-    MetadataIn,
-    MetadataDB,
     MetadataOut,
     MetadataPatch,
     patch_metadata,
+    MetadataDB
 )
+from app.models.pyobjectid import PyObjectId
 
 router = APIRouter()
 
 
 @router.post("/definition", response_model=MetadataDefinitionOut)
 async def save_metadata_definition(
-    definition_in: MetadataDefinitionIn,
-    user=Depends(get_current_user),
-    db: MongoClient = Depends(dependencies.get_db),
+        definition_in: MetadataDefinitionIn,
+        user=Depends(get_current_user),
+        db: MongoClient = Depends(dependencies.get_db),
 ):
     if (
-        md_def := await db["metadata.definitions"].find_one(
-            {"name": definition_in.name}
-        )
+            md_def := await MetadataDefinitionDB.find_one(MetadataDefinitionDB.name == definition_in.name)
     ) is not None:
         raise HTTPException(
             status_code=409,
             detail=f"Metadata definition named {definition_in.name} already exists.",
         )
 
-    md_def = MetadataDefinitionDB(**definition_in.dict(), creator=user)
-    new_md_def = await db["metadata.definitions"].insert_one(md_def.to_mongo())
-    found = await db["metadata.definitions"].find_one({"_id": new_md_def.inserted_id})
+    new_md_def = await MetadataDefinitionDB(**definition_in.dict(), creator=user).insert()
+    found = await MetadataDefinitionDB.find_one(MetadataDefinitionDB.id == new_md_def.inserted_id)
     md_def_out = MetadataDefinitionOut.from_mongo(found)
     return md_def_out
 
 
 @router.get("/definition", response_model=List[MetadataDefinitionOut])
 async def get_metadata_definition(
-    name: Optional[str] = None,
-    user=Depends(get_current_user),
-    db: MongoClient = Depends(dependencies.get_db),
-    skip: int = 0,
-    limit: int = 2,
+        name: Optional[str] = None,
+        user=Depends(get_current_user),
+        db: MongoClient = Depends(dependencies.get_db),
+        skip: int = 0,
+        limit: int = 2,
 ):
     definitions = []
     if name is None:
-        root_query = db["metadata.definitions"].find()
+        root_query = MetadataDefinitionDB.find_all()
     else:
-        root_query = db["metadata.definitions"].find({"name": name})
+        root_query = await MetadataDefinitionDB.find_one(MetadataDefinitionDB.name == name)
 
     for doc in await root_query.skip(skip).limit(limit).to_list(length=limit):
         definitions.append(MetadataDefinitionOut.from_mongo(doc))
@@ -71,11 +65,11 @@ async def get_metadata_definition(
 
 @router.patch("/{metadata_id}", response_model=MetadataOut)
 async def update_metadata(
-    metadata_in: MetadataPatch,
-    metadata_id: str,
-    user=Depends(get_current_user),
-    db: MongoClient = Depends(dependencies.get_db),
-    allow: bool = Depends(MetadataAuthorization("editor")),
+        metadata_in: MetadataPatch,
+        metadata_id: str,
+        user=Depends(get_current_user),
+        db: MongoClient = Depends(dependencies.get_db),
+        allow: bool = Depends(MetadataAuthorization("editor")),
 ):
     """Update metadata. Any fields provided in the contents JSON will be added or updated in the metadata. If context or
     agent should be changed, use PUT.
@@ -84,7 +78,7 @@ async def update_metadata(
         Metadata document that was updated
     """
     if (
-        md := await db["metadata"].find_one({"_id": PyObjectId(metadata_id)})
+            md := await MetadataDB.find_one(MetadataDB.id == PyObjectId(metadata_id))
     ) is not None:
         # TODO: Refactor this with permissions checks etc.
         contents = metadata_in.contents
@@ -96,17 +90,18 @@ async def update_metadata(
 
 @router.delete("/{metadata_id}")
 async def delete_metadata(
-    metadata_id: str,
-    user=Depends(get_current_user),
-    db: MongoClient = Depends(dependencies.get_db),
-    allow: bool = Depends(MetadataAuthorization("editor")),
+        metadata_id: str,
+        user=Depends(get_current_user),
+        db: MongoClient = Depends(dependencies.get_db),
+        allow: bool = Depends(MetadataAuthorization("editor")),
 ):
     """Delete metadata by specific ID."""
     if (
-        md := await db["metadata"].find_one({"_id": PyObjectId(metadata_id)})
+            md := await MetadataDB.find_one(MetadataDB.id == PyObjectId(metadata_id))
     ) is not None:
         # TODO: Refactor this with permissions checks etc.
         await db["metadata"].delete_one({"_id": PyObjectId(metadata_id)})
+        await MetadataDB(id=PyObjectId(metadata_id)).delete()
         return {"deleted": metadata_id}
     else:
         raise HTTPException(status_code=404, detail=f"Metadata {metadata_id} not found")
