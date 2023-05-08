9dbeb68ccd9c1d0c332ac95ddb6c48872a1d7b4a
Chen Wang <cwang138@illinois.edu>
2023-05-08 09:53:36 -0500

    codegen

--- frontend/src/openapi/v2/models/MetadataOut.ts
+++ frontend/src/openapi/v2/models/MetadataOut.ts
@@ -5,8 +5,21 @@
 import type { MetadataAgent } from './MetadataAgent';
 import type { MongoDBRef } from './MongoDBRef';
 
+/**
+ * Document Mapping class.
+ *
+ * Fields:
+ *
+ * - `id` - MongoDB document ObjectID "_id" field.
+ * Mapped to the PydanticObjectId class
+ *
+ * Inherited from:
+ *
+ * - Pydantic BaseModel
+ * - [UpdateMethods](https://roman-right.github.io/beanie/api/interfaces/#aggregatemethods)
+ */
 export type MetadataOut = {
-    id?: string;
+    _id?: string;
     context?: Array<string>;
     context_url?: string;
     definition?: string;
