d000bd5cda128ef31904de8935d61c8319513bef
Chen Wang <cwang138@illinois.edu>
2023-05-09 16:05:29 -0500

    codegen

--- frontend/src/openapi/v2/models/FolderOut.ts
+++ frontend/src/openapi/v2/models/FolderOut.ts
@@ -4,8 +4,21 @@
 
 import type { UserOut } from './UserOut';
 
+/**
+ * Document Mapping class.
+ *
+ * Fields:
+ *
+ * - `id` - MongoDB document ObjectID "_id" field.
+ * Mapped to the PydanticObjectId class
+ *
+ * Inherited from:
+ *
+ * - Pydantic BaseModel
+ * - [UpdateMethods](https://roman-right.github.io/beanie/api/interfaces/#aggregatemethods)
+ */
 export type FolderOut = {
-    id?: string;
+    _id?: string;
     name?: string;
     dataset_id: string;
     parent_folder?: string;
