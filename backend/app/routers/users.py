ca6fb2be82bfab2282d7bd79b2af96828701b401
Max Burnette <mburnet2@illinois.edu>
2023-05-10 10:32:58 -0500

    rename methods

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -17,7 +17,7 @@ router = APIRouter()
 
 
 @router.get("/keys", response_model=List[UserAPIKeyOut])
-async def generate_user_api_key(
+async def get_user_api_keys(
     db: MongoClient = Depends(dependencies.get_db),
     current_user=Depends(get_current_username),
     skip: int = 0,
@@ -47,7 +47,6 @@ async def generate_user_api_key(
 async def generate_user_api_key(
     name: str,
     mins: int = settings.local_auth_expiration,
-    db: MongoClient = Depends(dependencies.get_db),
     current_user=Depends(get_current_username),
 ):
     """Generate an API key that confers the user's privileges.
@@ -63,13 +62,12 @@ async def generate_user_api_key(
     user_key = UserAPIKey(user=current_user, key=unique_key, name=name)
     if mins > 0:
         user_key.expires = user_key.created + timedelta(minutes=mins)
-    db["user_keys"].insert_one(user_key.to_mongo())
-
+    await UserAPIKey.insert_one(user_key)
     return hashed_key
 
 
 @router.delete("/keys/{key_id}", response_model=UserAPIKeyOut)
-async def generate_user_api_key(
+async def delete_user_api_key(
     key_id: str,
     db: MongoClient = Depends(dependencies.get_db),
     current_user=Depends(get_current_username),
@@ -128,25 +126,3 @@ async def get_user_by_name(
     if user is not None:
         return UserOut.from_mongo(**user.dict())
     raise HTTPException(status_code=404, detail=f"User {username} not found")
-
-
-@router.post("/keys", response_model=str)
-async def generate_user_api_key(
-    mins: int = settings.local_auth_expiration,
-    db: MongoClient = Depends(dependencies.get_db),
-    current_user=Depends(get_current_username),
-):
-    """Generate an API key that confers the user's privileges.
-
-    Arguments:
-        mins -- number of minutes before expiration (0 for no expiration)
-    """
-    serializer = URLSafeSerializer(settings.local_auth_secret, salt="api_key")
-    unique_key = token_urlsafe(16)
-    hashed_key = serializer.dumps({"user": current_user, "key": unique_key})
-
-    user_key = UserAPIKey(user=current_user, key=unique_key)
-    if mins > 0:
-        user_key.expires = user_key.created + timedelta(minutes=mins)
-    await UserAPIKey.insert_one(user_key)
-    return hashed_key
