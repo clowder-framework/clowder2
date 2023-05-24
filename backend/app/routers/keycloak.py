ad79eef9e45ad49c93f1f586afd67dc78337e4b8
Chen Wang <cwang138@illinois.edu>
2023-05-24 12:35:02 -0500

    restore keycloak.py

--- backend/app/routers/keycloak.py
+++ backend/app/routers/keycloak.py
@@ -2,17 +2,21 @@ import json
 import logging
 
 import requests
-from fastapi import APIRouter, HTTPException, Security
+from bson import ObjectId
+from fastapi import APIRouter, HTTPException, Depends, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import jwt, ExpiredSignatureError, JWTError
 from keycloak.exceptions import KeycloakAuthenticationError, KeycloakGetError
+from pymongo import MongoClient
 from starlette.responses import RedirectResponse
 
+from app import dependencies
 from app.config import settings
 from app.keycloak_auth import (
     keycloak_openid,
     get_idp_public_key,
     retreive_refresh_token,
+    oauth2_scheme,
 )
 from app.models.tokens import TokenDB
 from app.models.users import UserIn, UserDB
@@ -37,6 +41,7 @@ async def login() -> RedirectResponse:
 @router.get("/logout")
 async def logout(
         credentials: HTTPAuthorizationCredentials = Security(security),
+        db: MongoClient = Depends(dependencies.get_db),
 ):
     """Logout of keycloak."""
     # get user info
@@ -44,15 +49,15 @@ async def logout(
     try:
         user_info = keycloak_openid.userinfo(access_token)
         if (
-                token_exist := await TokenDB.find_one({"email": user_info["email"]})
+                token_exist := await db["tokens"].find_one({"email": user_info["email"]})
         ) is not None:
             # log user out
             try:
                 keycloak_openid.logout(token_exist["refresh_token"])
 
                 # delete entry in the token database
-                await token_exist.delete()
-                return {"status": f"Successfully logged user: {user_info} out!"}
+                await db["tokens"].delete_one({"_id": ObjectId(token_exist["_id"])})
+                return {"status": f"Successfully log user: {user_info} out!"}
             except:
                 raise HTTPException(
                     status_code=403,
@@ -96,7 +101,7 @@ async def login(userIn: UserIn):
 
 @router.get("")
 async def auth(
-        code: str
+        code: str, db: MongoClient = Depends(dependencies.get_db)
 ) -> RedirectResponse:
     """Redirect endpoint Keycloak redirects to after login."""
     logger.info(f"In /api/v2/auth")
@@ -125,18 +130,18 @@ async def auth(
         hashed_password="",
         keycloak_id=keycloak_id,
     )
-    matched_user = await UserDB.find_one(UserDB.email == email)
-    if matched_user is None:
-        await user.insert()
+    if (await db["users"].find_one({"email": email})) is None:
+        await db["users"].insert_one(user.to_mongo())
 
     # store/update refresh token and link to that userid
-    token_exist = await TokenDB.find_one(TokenDB.email == email)
-    if token_exist is not None:
-        token_exist.refresh_token = token_body["refresh_token"]
-        await token_exist.save()
+    if (token_exist := await db["tokens"].find_one({"email": email})) is not None:
+        token_exist.update({"refresh_token": token_body["refresh_token"]})
+        await db["tokens"].replace_one(
+            {"_id": ObjectId(token_exist["_id"])}, token_exist
+        )
     else:
         token_created = TokenDB(email=email, refresh_token=token_body["refresh_token"])
-        await token_created.insert()
+        await db["tokens"].insert_one(token_created.to_mongo())
 
     # redirect to frontend
     auth_url = f"{settings.frontend_url}/auth"
@@ -149,6 +154,7 @@ async def auth(
 @router.get("/refresh_token")
 async def refresh_token(
         credentials: HTTPAuthorizationCredentials = Security(security),
+        db: MongoClient = Depends(dependencies.get_db),
 ):
     access_token = credentials.credentials
 
@@ -160,11 +166,11 @@ async def refresh_token(
             options={"verify_aud": False},
         )
         email = token_json["email"]
-        return await retreive_refresh_token(email)
+        return await retreive_refresh_token(email, db)
     except (ExpiredSignatureError, JWTError):
         # retreive the refresh token and try refresh
         email = jwt.get_unverified_claims(access_token)["email"]
-        return await retreive_refresh_token(email)
+        return await retreive_refresh_token(email, db)
     except KeycloakGetError as e:
         raise HTTPException(
             status_code=e.response_code,
@@ -178,28 +184,29 @@ async def refresh_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-# FIXME: we need to parse and return a consistent response
-# @router.get("/broker/{identity_provider}/token")
-# def get_idenity_provider_token(
-#     identity_provider: str, access_token: str = Security(oauth2_scheme)
-# ) -> Json:
-#     """Get identity provider JWT token from keyclok. Keycloak must be configured to store external tokens."""
-#     if identity_provider in settings.keycloak_ipds:
-#         idp_url = f"{settings.auth_base}/auth/realms/{settings.auth_realm}/broker/{identity_provider}/token"
-#         idp_headers = {
-#             "Content-Type": "application/x-www-form-urlencoded",
-#             "Authorization": f"Bearer {access_token}",
-#         }
-#         idp_token = requests.request("GET", idp_url, headers=idp_headers)
-#         # FIXME is there a better way to know if the token as expired and the above call did not go through?
-#         idp_token.raise_for_status()
-#         itp_token_body = json.loads(idp_token.content)
-#         return itp_token_body
-#     else:
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             detail={
-#                 "error_msg": f"Identy provider [{identity_provider}] not recognized."
-#             },
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
+
+@router.get("/broker/{identity_provider}/token")
+def get_idenity_provider_token(
+        identity_provider: str, access_token: str = Security(oauth2_scheme)
+):
+    """Get identity provider JWT token from keyclok. Keycloak must be configured to store external tokens."""
+    if identity_provider in settings.keycloak_ipds:
+        idp_url = f"{settings.auth_base}/auth/realms/{settings.auth_realm}/broker/{identity_provider}/token"
+        idp_headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Bearer {access_token}",
+        }
+        idp_token = requests.request("GET", idp_url, headers=idp_headers)
+        if idp_token.status_code == 200:
+            itp_token_body = json.loads(idp_token.content)
+            return itp_token_body
+        else:
+            raise HTTPException(status_code=400, detail=idp_token.text)
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "error_msg": f"Identy provider [{identity_provider}] not recognized."
+            },
+            headers={"WWW-Authenticate": "Bearer"},
+        )
