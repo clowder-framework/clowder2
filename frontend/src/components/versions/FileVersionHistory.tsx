0181283e630f8560dce9525ce61161fec3291141
Chen Wang <cwang138@illinois.edu>
2023-06-07 14:17:07 -0500

    add a width to the version history text part (#535)

--- frontend/src/components/versions/FileVersionHistory.tsx
+++ frontend/src/components/versions/FileVersionHistory.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import {Box, Button, List, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
-import {VersionChip} from "./VersionChip";
-import {parseDate} from "../../utils/common";
-import {FileVersion} from "../../openapi/v2";
-import {fileDownloaded} from "../../actions/file";
-import {useDispatch} from "react-redux";
+import {
+	Box,
+	Button,
+	List,
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+} from "@mui/material";
+import { VersionChip } from "./VersionChip";
+import { parseDate } from "../../utils/common";
+import { FileVersion } from "../../openapi/v2";
+import { fileDownloaded } from "../../actions/file";
+import { useDispatch } from "react-redux";
 
 type FileVersionHistoryProps = {
-	fileVersions: FileVersion[],
-	filename: string,
-}
+	fileVersions: FileVersion[];
+	filename: string;
+};
 
 export function FileVersionHistory(props: FileVersionHistoryProps) {
-	const {fileVersions, filename} = props;
+	const { fileVersions, filename } = props;
 	const dispatch = useDispatch();
-	const downloadFile = (fileId:string|undefined, filename:string|undefined, fileVersion:number|undefined) => dispatch(fileDownloaded(fileId, filename, fileVersion))
+	const downloadFile = (
+		fileId: string | undefined,
+		filename: string | undefined,
+		fileVersion: number | undefined
+	) => dispatch(fileDownloaded(fileId, filename, fileVersion));
 
 	return (
 		<Box className="infoCard">
@@ -22,20 +33,27 @@ export function FileVersionHistory(props: FileVersionHistoryProps) {
 				{
 					// sort by date decending
 					fileVersions.map((fileVersion) => {
-						const {version_num, creator, created} = fileVersion;
+						const { version_num, creator, created } = fileVersion;
 						return (
 							<ListItem key={version_num}>
 								<ListItemAvatar>
-									<VersionChip versionNumber={version_num}/>
+									<VersionChip versionNumber={version_num} />
 								</ListItemAvatar>
 								<ListItemText
-									primary={`Uploaded by ${creator != null ?
-										`${creator.first_name} ${creator.last_name}` : ""}`}
+									primary={`Uploaded by ${
+										creator != null
+											? `${creator.first_name} ${creator.last_name}`
+											: ""
+									}`}
 									secondary={`Uploaded on ${parseDate(created)}`}
+									sx={{ maxWidth: "38rem" }}
 								/>
-								<Button onClick={()=>{
-								   downloadFile(fileVersion.file_id, filename, version_num);
-								}}>
+								<Button
+									variant="contained"
+									onClick={() => {
+										downloadFile(fileVersion.file_id, filename, version_num);
+									}}
+								>
 									Download
 								</Button>
 								{/*TODO implement those actions*/}
