14edc4da8c6b891f4d931b562b19a329af1f22be
Chen Wang <cwang138@illinois.edu>
2023-06-07 14:23:36 -0500

    add chunkfilename

--- frontend/webpack.config.dev.js
+++ frontend/webpack.config.dev.js
@@ -4,15 +4,16 @@ import autoprefixer from "autoprefixer";
 import path from "path";
 import ESLintPlugin from "eslint-webpack-plugin";
 
-
 // eslint-disable-next-line no-console
-console.log(`the current CLOWDER_REMOTE_HOSTNAME environment variable is ${  process.env.CLOWDER_REMOTE_HOSTNAME}`);
+console.log(
+	`the current CLOWDER_REMOTE_HOSTNAME environment variable is ${process.env.CLOWDER_REMOTE_HOSTNAME}`
+);
 
 export default {
-	mode:"development",
+	mode: "development",
 	resolve: {
-		modules:["node_modules", "src"],
-		extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+		modules: ["node_modules", "src"],
+		extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
 	},
 	devtool: "source-map",
 	entry: [
@@ -28,22 +29,25 @@ export default {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		publicPath: "",
-		filename: "bundle.js"
+		filename: "[name].bundle.js",
+		chunkFilename: "[name].chunk.bundle.js",
 	},
 	plugins: [
 		new webpack.DefinePlugin({
 			"process.env": {
-				"NODE_ENV": JSON.stringify("development"),
+				NODE_ENV: JSON.stringify("development"),
 				// if left not set, it will default to same host/port as frontend
-				"CLOWDER_REMOTE_HOSTNAME": JSON.stringify(process.env.CLOWDER_REMOTE_HOSTNAME),
-				"APIKEY": JSON.stringify(process.env.APIKEY),
-				"KeycloakBaseURL": JSON.stringify(process.env.KeycloakBaseURL),
+				CLOWDER_REMOTE_HOSTNAME: JSON.stringify(
+					process.env.CLOWDER_REMOTE_HOSTNAME
+				),
+				APIKEY: JSON.stringify(process.env.APIKEY),
+				KeycloakBaseURL: JSON.stringify(process.env.KeycloakBaseURL),
 			},
-			__DEV__: true
+			__DEV__: true,
 		}),
 		new ESLintPlugin({
-			extensions: ["ts","tsx","js","jsx"],
-			exclude: ["node_modules", "dist", "build"]
+			extensions: ["ts", "tsx", "js", "jsx"],
+			exclude: ["node_modules", "dist", "build"],
 		}),
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoEmitOnErrorsPlugin(),
@@ -52,66 +56,68 @@ export default {
 			favicon: "./src/public/favicon.ico",
 			minify: {
 				removeComments: true,
-				collapseWhitespace: true
+				collapseWhitespace: true,
 			},
-			inject: true
+			inject: true,
 		}),
 		new webpack.LoaderOptionsPlugin({
 			debug: true,
 			options: {
 				sassLoader: {
-					includePaths: [path.resolve(__dirname, "src", "scss")]
+					includePaths: [path.resolve(__dirname, "src", "scss")],
 				},
 				context: "/",
-				postcss: [
-					autoprefixer(),
-				]
-			}
-		})
+				postcss: [autoprefixer()],
+			},
+		}),
 	],
 	module: {
 		rules: [
 			{
 				test: /\.[tj]sx?$/,
 				exclude: /node_modules/,
-				loader: "babel-loader"
+				loader: "babel-loader",
 			},
 			{
 				test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-				type: "asset/inline"
+				type: "asset/inline",
 			},
 			{
 				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				type: "asset/inline"
+				type: "asset/inline",
 			},
 			{
 				test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
-				type: "asset/inline"
+				type: "asset/inline",
 			},
-			{	test: /\.ico$/,
-				type: "asset/resource"
+			{
+				test: /\.ico$/,
+				type: "asset/resource",
 			},
 			{
 				test: /\.svg(\?v=\d+.\d+.\d+)?$/,
-				type: "asset/inline"
+				type: "asset/inline",
 			},
 			{
 				test: /\.(jpe?g|png|gif)$/i,
-				type: "asset/resource"
+				type: "asset/resource",
 			},
 			{
 				test: /(\.css|\.scss)$/i,
-				use:[
+				use: [
 					"style-loader",
 					"css-loader",
-					{ loader: "postcss-loader", options: { postcssOptions: { plugins: ["autoprefixer"] }}},
-					{ loader: "sass-loader", options: { sourceMap: true } }
-				]
+					{
+						loader: "postcss-loader",
+						options: { postcssOptions: { plugins: ["autoprefixer"] } },
+					},
+					{ loader: "sass-loader", options: { sourceMap: true } },
+				],
 			},
 			// {
 			// 	test: /\.json$/,
 			// 	loader: "json-loader"
 			// }
-		]
+		],
 	},
 };
