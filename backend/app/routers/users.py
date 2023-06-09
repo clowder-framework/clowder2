9931134c7613f4e60e99f30f1af4d214e5882c9c
Chen Wang <cwang138@illinois.edu>
2023-06-09 10:17:21 -0500

    add endpoint match user email and first last name

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,3 +1,4 @@
+import re
 from datetime import timedelta
 from secrets import token_urlsafe
 from typing import List
@@ -17,10 +18,10 @@ router = APIRouter()
 
 @router.get("/keys", response_model=List[UserAPIKeyOut])
 async def generate_user_api_key(
-    db: MongoClient = Depends(dependencies.get_db),
-    current_user=Depends(get_current_username),
-    skip: int = 0,
-    limit: int = 10,
+        db: MongoClient = Depends(dependencies.get_db),
+        current_user=Depends(get_current_username),
+        skip: int = 0,
+        limit: int = 10,
 ):
     """List all api keys that user has created
 
@@ -30,12 +31,12 @@ async def generate_user_api_key(
     """
     apikeys = []
     for doc in (
-        await db["user_keys"]
-        .find({"user": current_user})
-        .sort([("created", DESCENDING)])
-        .skip(skip)
-        .limit(limit)
-        .to_list(length=limit)
+            await db["user_keys"]
+                    .find({"user": current_user})
+                    .sort([("created", DESCENDING)])
+                    .skip(skip)
+                    .limit(limit)
+                    .to_list(length=limit)
     ):
         apikeys.append(UserAPIKeyOut.from_mongo(doc))
 
@@ -44,10 +45,10 @@ async def generate_user_api_key(
 
 @router.post("/keys", response_model=str)
 async def generate_user_api_key(
-    name: str,
-    mins: int = settings.local_auth_expiration,
-    db: MongoClient = Depends(dependencies.get_db),
-    current_user=Depends(get_current_username),
+        name: str,
+        mins: int = settings.local_auth_expiration,
+        db: MongoClient = Depends(dependencies.get_db),
+        current_user=Depends(get_current_username),
 ):
     """Generate an API key that confers the user's privileges.
 
@@ -69,9 +70,9 @@ async def generate_user_api_key(
 
 @router.delete("/keys/{key_id}", response_model=UserAPIKeyOut)
 async def generate_user_api_key(
-    key_id: str,
-    db: MongoClient = Depends(dependencies.get_db),
-    current_user=Depends(get_current_username),
+        key_id: str,
+        db: MongoClient = Depends(dependencies.get_db),
+        current_user=Depends(get_current_username),
 ):
     """Delete API keys given ID
 
@@ -96,7 +97,7 @@ async def generate_user_api_key(
 
 @router.get("", response_model=List[UserOut])
 async def get_users(
-    db: MongoClient = Depends(dependencies.get_db), skip: int = 0, limit: int = 2
+        db: MongoClient = Depends(dependencies.get_db), skip: int = 0, limit: int = 2
 ):
     users = []
     for doc in await db["users"].find().skip(skip).limit(limit).to_list(length=limit):
@@ -104,10 +105,24 @@ async def get_users(
     return users
 
 
+@router.get("/search", response_model=List[UserOut])
+async def search_users(
+        text: str,
+        db: MongoClient = Depends(dependencies.get_db),
+        skip: int = 0,
+        limit: int = 2
+):
+    query_regx = re.compile(text, re.IGNORECASE)
+    users = []
+    for doc in await db["users"].find({"email": query_regx}).skip(skip).limit(limit).to_list(length=limit):
+        users.append(UserOut(**doc))
+    return users
+
+
 @router.get("/profile", response_model=UserOut)
 async def get_profile(
-    username=Depends(get_current_username),
-    db: MongoClient = Depends(dependencies.get_db),
+        username=Depends(get_current_username),
+        db: MongoClient = Depends(dependencies.get_db),
 ):
     if (user := await db["users"].find_one({"email": username})) is not None:
         return UserOut.from_mongo(user)
@@ -123,7 +138,7 @@ async def get_user(user_id: str, db: MongoClient = Depends(dependencies.get_db))
 
 @router.get("/username/{username}", response_model=UserOut)
 async def get_user_by_name(
-    username: str, db: MongoClient = Depends(dependencies.get_db)
+        username: str, db: MongoClient = Depends(dependencies.get_db)
 ):
     if (user := await db["users"].find_one({"email": username})) is not None:
         return UserOut.from_mongo(user)
