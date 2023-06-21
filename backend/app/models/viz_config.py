8e583e2f5d45f86192ea95573e088d73a1c03227
toddn <tcnichol@illinois.edu>
2023-06-21 16:16:18 -0500

    initial commit viz config class

---
+++ backend/app/models/viz_config.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Optional, List
+
+from beanie import Document, View, PydanticObjectId
+from pydantic import Field, BaseModel
+
+from app.models.authorization import AuthorizationDB
+from app.models.listeners import ExtractorInfo
+from app.models.pyobjectid import PyObjectId
+from app.models.users import UserOut
+
+
+class VizConfigBase(BaseModel):
+    file_id: PydanticObjectId
+    extractor_info: ExtractorInfo
+    visualization: PydanticObjectId
+
+class VizConfigDB(Document, VizConfigBase):
+    class Settings:
+        name = "vizconfig"
+
+
+
+class VizConfigOut(VizConfigDB):
+    class Config:
+        fields = {"id": "id"}
