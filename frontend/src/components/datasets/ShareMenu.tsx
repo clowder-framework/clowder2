075e6af3724f0d82527cebf1956a7a5d179d0a35
toddn <tcnichol@illinois.edu>
2023-07-20 17:21:58 -0500

    share menu added

---
+++ frontend/src/components/datasets/ShareMenu.tsx
@@ -0,0 +1,118 @@
+import {Box, Button, ListItemIcon, ListItemText, Menu, MenuItem} from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import React, {useEffect, useState} from "react";
+import {ActionModal} from "../dialog/ActionModal";
+import {datasetDeleted, fetchFilesInDataset} from "../../actions/dataset";
+import {fetchGroups} from "../../actions/group";
+import {useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {MoreHoriz} from "@material-ui/icons";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ShareIcon from '@mui/icons-material/Share';
+import ShareDatasetModal from "./ShareDatasetModal"
+import ShareGroupDatasetModal from "./ShareGroupDatasetModal";
+
+type ActionsMenuProps = {
+	datasetId: string,
+	datasetName: string
+}
+
+export const ShareMenu = (props: ActionsMenuProps): JSX.Element => {
+	const {datasetId, datasetName} = props;
+
+	// use history hook to redirect/navigate between routes
+	const history = useNavigate();
+
+	// redux
+	const dispatch = useDispatch();
+	const deleteDataset = (datasetId: string | undefined) => dispatch(datasetDeleted(datasetId));
+	const listGroups = () => dispatch(fetchGroups(0, 21));
+
+	// component did mount
+	useEffect(() => {
+		listGroups();
+	}, []);
+
+	// state
+	const [deleteDatasetConfirmOpen, setDeleteDatasetConfirmOpen] = useState(false);
+	const [sharePaneOpen, setSharePaneOpen] = useState(false);
+	const [shareGroupPaneOpen, setShareGroupPaneOpen] = useState(false);
+
+
+
+	// delete dataset
+	const deleteSelectedDataset = () => {
+		if (datasetId) {
+			deleteDataset(datasetId);
+		}
+		setDeleteDatasetConfirmOpen(false);
+		// Go to Explore page
+		history("/");
+	}
+
+	const [anchorEl, setAnchorEl] = React.useState<Element | null>(null);
+
+	const handleOptionClick = (event: React.MouseEvent<any>) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleOptionClose = () => {
+		setAnchorEl(null);
+	};
+
+    const handleShareClose = () => {
+        setSharePaneOpen(false);
+    }
+
+      const handleShareGroupClose = () => {
+        setShareGroupPaneOpen(false);
+    }
+
+	return (
+		<Box>
+			<ActionModal actionOpen={deleteDatasetConfirmOpen} actionTitle="Are you sure?"
+						 actionText="Do you really want to delete this dataset? This process cannot be undone."
+						 actionBtnName="Delete" handleActionBtnClick={deleteSelectedDataset}
+						 handleActionCancel={() => {
+							 setDeleteDatasetConfirmOpen(false);
+						 }}/>
+
+ 		        <ShareDatasetModal open={sharePaneOpen} handleClose={handleShareClose} datasetName={datasetName}/>
+			 	<ShareGroupDatasetModal open={shareGroupPaneOpen} handleClose={handleShareGroupClose} datasetName={datasetName}/>
+
+			<Button variant="outlined" onClick={handleOptionClick}
+					endIcon={<ArrowDropDownIcon/>}>
+				Share
+			</Button>
+			<Menu
+				id="simple-menu"
+				anchorEl={anchorEl}
+				keepMounted
+				open={Boolean(anchorEl)}
+				onClose={handleOptionClose}
+			>
+					<MenuItem
+							onClick={() => {
+								handleOptionClose();
+								setSharePaneOpen(true);
+							}
+						}>
+							<ListItemIcon>
+								<ShareIcon fontSize="small" />
+							</ListItemIcon>
+							<ListItemText>Share</ListItemText>
+					</MenuItem>
+					<MenuItem
+							onClick={() => {
+								handleOptionClose();
+								setShareGroupPaneOpen(true);
+							}
+						}>
+							<ListItemIcon>
+								<ShareIcon fontSize="small" />
+							</ListItemIcon>
+							<ListItemText>Share With Group</ListItemText>
+					</MenuItem>
+			</Menu>
+		</Box>)
+}
