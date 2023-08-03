7e55e03a68eaef5cbb3f6ea3c9bc639c862d11b1
toddn <tcnichol@illinois.edu>
2023-08-03 16:21:01 -0500

    placeholder modal does not work

---
+++ frontend/src/components/files/SelectVersionModal.tsx
@@ -0,0 +1,114 @@
+import React, {useState} from "react";
+import {RootState} from "../../types/data";
+import {
+	Alert,
+	Autocomplete,
+	Box,
+	Button, Collapse,
+	Container,
+	Dialog, DialogActions, DialogContent,
+	DialogTitle,
+	Divider,
+	FormControl, IconButton,
+	InputLabel,
+	MenuItem,
+	Select, TextField, Typography
+} from "@mui/material";
+import Form from "@rjsf/material-ui";
+
+import fileSchema from "../../schema/fileSchema.json";
+import {FormProps} from "@rjsf/core";
+import CloseIcon from "@mui/icons-material/Close";
+import {useDispatch} from "react-redux";
+
+
+type SelectVersionModalProps ={
+	open: boolean;
+	handleClose: any;
+	selected_version: number;
+	fileVersions: any;
+}
+
+export const SelectVersionModal: React.FC<SelectVersionModalProps> = (props: SelectVersionModalProps) => {
+
+	const {open, handleClose, selected_version, fileVersions, options} = props;
+	const dispatch = useDispatch();
+	const [selectedVersion, setSelectedVersion] = useState(selected_version);
+	const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+
+	const changeVersion = () => {
+		console.log('changing version');
+	}
+
+	return (
+		<Container>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				fullWidth={true}
+				maxWidth="md"
+				sx={{
+					".MuiPaper-root": {
+						padding: "2em",
+					},
+				}}
+			>
+				<DialogTitle>Change the Version</DialogTitle>
+				<Divider />
+				<DialogContent>
+					<Typography>Invite people to collaborate</Typography>
+						as
+						<FormControl variant="outlined" sx={{ m: 1, minWidth: 120 }}>
+							<InputLabel id="demo-simple-select-label">Status</InputLabel>
+							<Select
+								labelId="demo-simple-select-label"
+								id="demo-simple-select"
+								value={selectedVersion}
+								defaultValue={"viewer"}
+								label="Status"
+								onChange={(event, value) => {
+									setSelectedVersion(event.target.value);
+								}}
+							>
+								<MenuItem value="owner">Owner</MenuItem>
+								<MenuItem value="editor">Editor</MenuItem>
+								<MenuItem value="uploader">Uploader</MenuItem>
+								<MenuItem value="viewer">Viewer</MenuItem>
+							</Select>
+						</FormControl>
+					<Button
+						variant="contained"
+						sx={{ marginTop: 1 }}
+						onClick={changeVersion}
+					>
+						Share
+					</Button>
+					<Collapse in={showSuccessAlert}>
+						<br />
+						<Alert
+							severity="success"
+							action={
+								<IconButton
+									aria-label="close"
+									color="inherit"
+									size="small"
+									onClick={() => {
+										setShowSuccessAlert(false);
+									}}
+								>
+									<CloseIcon fontSize="inherit" />
+								</IconButton>
+							}
+							sx={{ mb: 2 }}
+						>
+							Successfully added role!
+						</Alert>
+					</Collapse>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Close</Button>
+				</DialogActions>
+			</Dialog>
+		</Container>
+	);
+};
--- frontend/src/components/versions/VersionChip.tsx
+++ frontend/src/components/versions/VersionChip.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import {Chip} from "@mui/material";
+import {SelectVersionModal} from "../files/SelectVersionModal";
+import ShareDatasetModal from "../datasets/ShareDatasetModal";
 
 
 type VersionChipProps = {
@@ -14,18 +16,33 @@ export function VersionChip(props: VersionChipProps) {
 
 	const [changeVersion, setOpenChangeVersion] = useState(false);
 
+	const [changeVersionOpen, setChangeVersionOpen] = useState(false);
+
 	const clickChip = () => {
 		console.log('clicked the chip')
 		console.log(versionNumber, changeVersion);
 		setOpenChangeVersion(true);
 		console.log(changeVersion)
+		setChangeVersionOpen(true);
 	}
 
+	const handleVersionSelectClose = () => {
+        setOpenChangeVersion(false);
+    }
+
 	return (
-		<Chip label={`V${versionNumber ?? ""}`}
+		<>
+				 <SelectVersionModal open={changeVersionOpen}
+							 handleClose={handleVersionSelectClose}
+							 selected_version={1}
+							 fileVersions={versionNumbers}
+		 	/>
+			<Chip label={`V${versionNumber ?? ""}`}
 			  clickable={true}
 			  onClick={clickChip}
-		/>
+			/>
+		</>
+
 		// TODO can make this a clickable item
 		// <Chip label={`V${versionNumber}`} component="a" href="/" clickable/> : <></>
 	);
