5fa24b3dad17da6b6a2b36a2bc2492644eba435a
toddn <tcnichol@illinois.edu>
2023-08-04 12:04:51 -0500

    how to assign the selected_version_num ?

--- frontend/src/components/files/SelectVersionModal.tsx
+++ frontend/src/components/files/SelectVersionModal.tsx
@@ -19,7 +19,7 @@ import Form from "@rjsf/material-ui";
 import fileSchema from "../../schema/fileSchema.json";
 import {FormProps} from "@rjsf/core";
 import CloseIcon from "@mui/icons-material/Close";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {FileVersion} from "../../openapi/v2";
 
 
@@ -34,13 +34,20 @@ export const SelectVersionModal: React.FC<SelectVersionModalProps> = (props: Sel
 
 	const {open, handleClose, selected_version, fileVersions, options} = props;
 	const dispatch = useDispatch();
+	const version_num = useSelector( (state: RootState) => state.file.fileSummary.version_num);
+	const all_versions = useSelector( (state: RootState) => state.file.fileVersions);
+	const current_selected_version = useSelector((state: RootState) => state.file.selected_version_num);
+	console.log('version num frilm file summary', version_num, selected_version);
 	const [selectedVersion, setSelectedVersion] = useState(selected_version);
 	const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-
+	if (current_selected_version === undefined) {
+		console.log('current selected version is undefined');
+	}
 	console.log('file versions', fileVersions)
 	const changeVersion = () => {
 		console.log('changing version');
 		console.log(selectedVersion);
+		current_selected_version = selectedVersion;
 	}
 
 	return (
@@ -72,7 +79,7 @@ export const SelectVersionModal: React.FC<SelectVersionModalProps> = (props: Sel
 									setSelectedVersion(event.target.value);
 								}}
 							>
-								{fileVersions.map((fileVersion,idx) => {
+								{all_versions.map((fileVersion,idx) => {
 									return <MenuItem value={fileVersion.version_num}>{fileVersion.version_num}</MenuItem>
 								})}
 								{/*<MenuItem value="owner">Owner</MenuItem>*/}
--- frontend/src/types/data.ts
+++ frontend/src/types/data.ts
@@ -199,7 +199,7 @@ export interface FileState {
 	previews: FilePreview[];
 	fileVersions: FileVersion[];
 	fileRole: AuthorizationBase;
-	version_num: number;
+	selected_version_num: number;
 }
 
 export interface UserState {
