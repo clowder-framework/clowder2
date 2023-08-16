cb0f6c4c5337f81875f41edbaf035b5bff7adbe0
Chen Wang <cwang138@illinois.edu>
2023-08-16 10:25:16 -0500

    simplify the metadata agent table

--- frontend/src/components/metadata/ListenerContents.tsx
+++ frontend/src/components/metadata/ListenerContents.tsx
@@ -1,22 +1,13 @@
 import React from "react";
-import {Box, Typography} from "@mui/material";
-import {theme} from "../../theme";
-import {parseDate} from "../../utils/common";
-
-const textStyle = {
-	fontWeight: "normal",
-	fontSize: "16x",
-	color: theme.palette.primary,
-};
+import ReactJson from "react-json-view";
 
 type ListenerContentEntry = {
-	content: any,
-}
-
-
+	content: any;
+};
 
 export const ListenerContents = (props: ListenerContentEntry) => {
-	const {content} = props;
+	const { content } = props;
 
-	return <div><pre>{JSON.stringify(content, null, 2)}</pre></div>
-}
+	// return <div><pre>{JSON.stringify(content, null, 2)}</pre></div>
+	return <ReactJson src={content} />;
+};
