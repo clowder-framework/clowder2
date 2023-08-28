6f997c9071c950787eb21348c5c102bdecf22ea8
Todd Nicholson <40038535+tcnichol@users.noreply.github.com>
2023-08-28 09:36:24 -0500

    wording change Visualization Information (#670)

--- frontend/src/components/visualizations/VisualizationDataDetail.tsx
+++ frontend/src/components/visualizations/VisualizationDataDetail.tsx
@@ -32,7 +32,7 @@ export function VisualizationDataDetail(props: FileAboutProps) {
 	return (
 		<>
 			<Typography variant="h5" gutterBottom>
-				Details
+				Visualization Information
 			</Typography>
 			<StackedList keyValues={details} />
 		</>
