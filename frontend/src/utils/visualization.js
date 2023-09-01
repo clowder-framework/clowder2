15c8b6ba102f274b6705bb81d35fe03e5397c97f
Chen Wang <cwang138@illinois.edu>
2023-09-01 10:58:10 -0500

    rewrite download thumbnail

--- frontend/src/utils/visualization.js
+++ frontend/src/utils/visualization.js
@@ -1,31 +1,8 @@
 import { getHeader } from "./common";
 import config from "../app.config";
 
-export async function downloadThumbnail(thumbnailId, title = null) {
-	const url = `${config.hostname}/api/v2/thumbnails/${thumbnailId}`;
-
-	const authHeader = getHeader();
-	const response = await fetch(url, {
-		method: "GET",
-		mode: "cors",
-		headers: authHeader,
-	});
-	if (response.status === 200) {
-		const blob = await response.blob();
-		if (window.navigator.msSaveOrOpenBlob) {
-			window.navigator.msSaveBlob(blob, thumbnailId);
-			return null;
-		} else {
-			return window.URL.createObjectURL(blob);
-		}
-	} else if (response.status === 401) {
-		// TODO handle error
-		// logout();
-		return null;
-	} else {
-		// TODO handle error
-		return null;
-	}
+export function downloadThumbnail(thumbnailId) {
+	return `${config.hostname}/api/v2/thumbnails/${thumbnailId}`;
 }
 
 export function generateVisDataDownloadUrl(visualizationId) {
