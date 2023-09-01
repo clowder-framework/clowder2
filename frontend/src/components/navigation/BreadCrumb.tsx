ac62453a4c3adde93475124be5a9924ff7413b98
Chen Wang <cwang138@illinois.edu>
2023-09-01 15:21:01 -0500

    rewrite breadcrumb to disable the last item also rewrite the path logic

--- frontend/src/components/navigation/BreadCrumb.tsx
+++ frontend/src/components/navigation/BreadCrumb.tsx
@@ -1,23 +1,39 @@
 import React from "react";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-import {Button, Link} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import { Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Typography from "@mui/material/Typography";
 
 type BreadCrumbProps = {
-	paths: any
-}
-export const MainBreadcrumbs: React.FC<BreadCrumbProps> = (props:BreadCrumbProps) => {
+	paths: any;
+};
+export const MainBreadcrumbs: React.FC<BreadCrumbProps> = (
+	props: BreadCrumbProps
+) => {
 	const { paths } = props;
 	const history = useNavigate();
 	return (
-			<Breadcrumbs aria-lab
-						 el="breadcrumb">
-				{
-					paths.map((path:any, index:number) => {
-							return (<Link component="button" style={{ textDecoration: "none"}} key={index} onClick={() => history(path["url"])}>{path["name"]}
-							</Link>);
-					})
+		<Breadcrumbs>
+			{paths.map((path: any, index: number) => {
+				if (index === paths.length - 1) {
+					return (
+						<Typography key={index} color="text.primary">
+							{path["name"]}
+						</Typography>
+					);
+				} else {
+					return (
+						<Link
+							component="button"
+							style={{ textDecoration: "none" }}
+							key={index}
+							onClick={() => history(path["url"])}
+						>
+							{path["name"]}
+						</Link>
+					);
 				}
-			</Breadcrumbs>
+			})}
+		</Breadcrumbs>
 	);
 };
