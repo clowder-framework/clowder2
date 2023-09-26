cccbbcb5b2f9d16b3c8b8c8470a539363e4633e7
Chen Wang <cwang138@illinois.edu>
2023-09-26 12:30:34 -0500

    rewrite iframe to html

---
+++ frontend/src/components/visualizations/html/Html.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useRef, useState} from "react";
+import {downloadVisData, fileDownloaded,} from "../../../utils/visualization";
+import {readTextFromFile} from "../../../utils/common";
+
+type htmlProps = {
+	fileId?: string;
+	visualizationId?: string;
+};
+
+export default function Html(props: htmlProps) {
+	const { fileId, visualizationId } = props;
+	const divRef = useRef(null);
+	const isFirstRender = useRef(true);
+
+	const [html, setHtml] = useState("");
+
+	useEffect(() => {
+		const processBlob = async () => {
+			try {
+				let blob;
+				if (visualizationId) {
+					blob = await downloadVisData(visualizationId);
+				} else {
+					blob = await fileDownloaded(fileId, 0);
+				}
+				const file = new File([blob], "text.tmp");
+				const text = await readTextFromFile(file);
+				setHtml(text);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+
+		processBlob();
+	}, [visualizationId, fileId]);
+
+	useEffect(() => {
+	if (!html || !divRef.current) throw new Error("html prop can't be null");
+	if (!isFirstRender.current) return;
+	isFirstRender.current = false;
+
+	const slotHtml = document.createRange().createContextualFragment(html);
+	divRef.current.innerHTML = ""; // Clear the container
+	divRef.current.appendChild(slotHtml); // Append the new content
+	}, [html, divRef])
+
+  return(<div ref={divRef} style={{ width: "100%", height: "50em", overflow: "auto" }}/>);
+}
