d40758b60710b88dfecce3cf60085f10b615ff3b
Chen Wang <cwang138@illinois.edu>
2023-10-02 11:13:28 -0500

    add search and get endpoint

--- backend/app/routers/metadata.py
+++ backend/app/routers/metadata.py
@@ -1,5 +1,8 @@
 from typing import Optional, List
 
+from beanie import PydanticObjectId
+from beanie.odm.operators.find.evaluation import RegEx
+from beanie.odm.operators.find.logical import Or
 from elasticsearch import Elasticsearch
 from fastapi import (
     APIRouter,
@@ -94,6 +97,49 @@ async def delete_metadata_definition(
             status_code=404,
             detail=f"Metadata definition {metadata_definition_id} not found",
         )
+
+
+@router.get("/search/{search_term}", response_model=List[MetadataDefinitionOut])
+async def search_metadata_definition(
+    search_term: str,
+    skip: int = 0,
+    limit: int = 10,
+    user=Depends(get_current_user),
+):
+    """Search all metadata definition in the db based on text.
+
+    Arguments:
+        text -- any text matching name or description
+        skip -- number of initial records to skip (i.e. for pagination)
+        limit -- restrict number of records to be returned (i.e. for pagination)
+    """
+
+    mdds = await MetadataDefinitionDB.find(
+        Or(
+            RegEx(field=MetadataDefinitionDB.name, pattern=search_term),
+            RegEx(field=MetadataDefinitionDB.description, pattern=search_term),
+            RegEx(field=MetadataDefinitionDB.context, pattern=search_term),
+        ),
+        skip=skip,
+        limit=limit,
+    ).to_list()
+
+    return [mdd.dict() for mdd in mdds]
+
+
+@router.get("/{metadata_definition_id}", response_model=MetadataDefinitionOut)
+async def get_metadata_definition(
+    metadata_definition_id: str,
+    user=Depends(get_current_user),
+):
+    if (
+        mdd := await MetadataDefinitionDB.get(PydanticObjectId(metadata_definition_id))
+    ) is not None:
+        return mdd.dict()
+    raise HTTPException(
+        status_code=404,
+        detail=f"Metadata definition {metadata_definition_id} not found",
+    )
 
 
 @router.patch("/{metadata_id}", response_model=MetadataOut)
--- frontend/src/openapi/v2/services/MetadataService.ts
+++ frontend/src/openapi/v2/services/MetadataService.ts
@@ -81,6 +81,56 @@ export class MetadataService {
     }
 
     /**
+     * Search Metadata Definition
+     * Search all metadata definition in the db based on text.
+     *
+     * Arguments:
+     * text -- any text matching name or description
+     * skip -- number of initial records to skip (i.e. for pagination)
+     * limit -- restrict number of records to be returned (i.e. for pagination)
+     * @param searchTerm
+     * @param skip
+     * @param limit
+     * @returns MetadataDefinitionOut Successful Response
+     * @throws ApiError
+     */
+    public static searchMetadataDefinitionApiV2MetadataSearchSearchTermGet(
+        searchTerm: string,
+        skip?: number,
+        limit: number = 10,
+    ): CancelablePromise<Array<MetadataDefinitionOut>> {
+        return __request({
+            method: 'GET',
+            path: `/api/v2/metadata/search/${searchTerm}`,
+            query: {
+                'skip': skip,
+                'limit': limit,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+
+    /**
+     * Get Metadata Definition
+     * @param metadataDefinitionId
+     * @returns MetadataDefinitionOut Successful Response
+     * @throws ApiError
+     */
+    public static getMetadataDefinitionApiV2MetadataMetadataDefinitionIdGet(
+        metadataDefinitionId: string,
+    ): CancelablePromise<MetadataDefinitionOut> {
+        return __request({
+            method: 'GET',
+            path: `/api/v2/metadata/${metadataDefinitionId}`,
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+
+    /**
      * Delete Metadata
      * Delete metadata by specific ID.
      * @param metadataId
