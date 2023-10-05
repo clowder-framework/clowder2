a2f3c1cc74b22073ec7af62b21fa5e0e05d990f2
Chen Wang <cwang138@illinois.edu>
2023-10-05 16:19:53 -0500

    fix the content type guess

--- backend/app/routers/utils.py
+++ backend/app/routers/utils.py
@@ -1,13 +1,12 @@
 import mimetypes
-import io
 from typing import Optional
 
 from app.models.files import ContentType
 
 
 def get_content_type(
-    filename: str,
-    content_type: Optional[str] = None,
+        filename: str,
+        content_type: Optional[str] = None,
 ):
     """Returns ContentType object given a content_tyoe, also guessed the content_type if none is provided
 
@@ -19,5 +18,8 @@ def get_content_type(
     if content_type is None:
         content_type = mimetypes.guess_type(filename)
         content_type = content_type[0] if len(content_type) > 1 else content_type
+        # If still cant guess the content_type, set it to default
+        if content_type is None:
+            content_type = "application/octet-stream"
     type_main = content_type.split("/")[0] if type(content_type) is str else "N/A"
     return ContentType(content_type=content_type, main_type=type_main)
