85a75fcf2bece64e2528b5d3a64c4e7f73bc0a2c
toddn <tcnichol@illinois.edu>
2023-11-26 14:09:06 -0600

    tests pass for public datasets, need tests for authenticated added

--- backend/app/tests/test_elastic_search.py
+++ backend/app/tests/test_elastic_search.py
@@ -55,7 +55,7 @@ updated_dummy_dataset_record = {
     }
 }
 
-dummy_pubilc_file_record = {
+dummy_public_file_record = {
     "name": "public test file",
     "creator": "xyz",
     "created": datetime.now(),
@@ -94,6 +94,7 @@ updated_dummy_public_dataset_record = {
         "created": datetime.now(),
         "modified": 1,
         "download": 0,
+        "status": "PUBLIC",
     }
 }
 
@@ -188,14 +189,23 @@ async def test_public_files():
             settings.elasticsearch_setting,
             indexSettings.es_mappings,
         )
-        insert_record(es, settings.elasticsearch_index, dummy_pubilc_file_record, 1)
+        insert_record(es, settings.elasticsearch_index, dummy_public_file_record, 1)
         time.sleep(1)
         dummy_file_query = []
+
+        user_public_clause = {
+            "bool": {
+                "should": [
+                    {"term": {"creator": "xyz"}},
+                    {"term": {"status": "PUBLIC"}},
+                ]
+            }
+        }
         # header
         dummy_file_query.append({"index": settings.elasticsearch_index})
         # body
         # dummy_file_query.append({"query": {"match": {"creator": "xyz"}}})
-        dummy_file_query.append({"query": {"match": {"status": "PUBLIC"}}})
+        dummy_file_query.append({"query": user_public_clause})
         file_query = ""
         for each in dummy_file_query:
             file_query += "%s \n" % json.dumps(each)
@@ -207,7 +217,7 @@ async def test_public_files():
         )
 
         # check for update to the record
-        update_record(es, settings.elasticsearch_index, updated_dummy_file_record, 1)
+        update_record(es, settings.elasticsearch_index, updated_dummy_public_file_record, 1)
         time.sleep(1)
         result = search_index(es, settings.elasticsearch_index, file_query)
         assert (
@@ -219,41 +229,48 @@ async def test_public_files():
         delete_index(es, settings.elasticsearch_index)
 
 
-# @pytest.mark.asyncio
-# async def test_public_datasets():
-#     # TODO: Replace this with actual file upload and search, not directly inserting record to ES
-#     es = await connect_elasticsearch()
-#     if es is not None:
-#         create_index(
-#             es,
-#             settings.elasticsearch_index,
-#             settings.elasticsearch_setting,
-#             indexSettings.es_mappings,
-#         )
-#         insert_record(es, settings.elasticsearch_index, dummy_public_dataset_record, 1)
-#         time.sleep(1)
-#         dummy_dataset_query = []
-#         # header
-#         dummy_dataset_query.append({"index": settings.elasticsearch_index})
-#         # body
-#         dummy_dataset_query.append({"query": {"match": {"creator": "abcd"}}})
-#         dummy_dataset_query.append({"query": {"match": {"public": True}}})
-#         dataset_query = ""
-#         for each in dummy_dataset_query:
-#             dataset_query += "%s \n" % json.dumps(each)
-#         result = search_index(es, settings.elasticsearch_index, dataset_query)
-#         assert (
-#             result.body["responses"][0]["hits"]["hits"][0]["_source"]["creator"]
-#             == "abcd"
-#         )
-#
-#         # check for update to the record
-#         update_record(es, settings.elasticsearch_index, updated_dummy_dataset_record, 1)
-#         time.sleep(1)
-#         result = search_index(es, settings.elasticsearch_index, dataset_query)
-#         assert (
-#             result.body["responses"][0]["hits"]["hits"][0]["_source"]["name"]
-#             == "test dataset 2"
-#         )
-#         delete_document_by_id(es, settings.elasticsearch_index, 1)
-#         delete_index(es, settings.elasticsearch_index)
+@pytest.mark.asyncio
+async def test_public_datasets():
+    # TODO: Replace this with actual file upload and search, not directly inserting record to ES
+    es = await connect_elasticsearch()
+    if es is not None:
+        create_index(
+            es,
+            settings.elasticsearch_index,
+            settings.elasticsearch_setting,
+            indexSettings.es_mappings,
+        )
+        insert_record(es, settings.elasticsearch_index, dummy_public_dataset_record, 1)
+        time.sleep(1)
+        dummy_dataset_query = []
+        # header
+        dummy_dataset_query.append({"index": settings.elasticsearch_index})
+        # body
+        query = {
+            "bool": {
+                "should": [
+                    {"term": {"creator": "abcd"}},
+                    {"term": {"status": "PUBLIC"}},
+                ]
+            }
+        }
+        dummy_dataset_query.append({"query": query})
+        dataset_query = ""
+        for each in dummy_dataset_query:
+            dataset_query += "%s \n" % json.dumps(each)
+        result = search_index(es, settings.elasticsearch_index, dataset_query)
+        assert (
+            result.body["responses"][0]["hits"]["hits"][0]["_source"]["creator"]
+            == "abcd"
+        )
+
+        # check for update to the record
+        update_record(es, settings.elasticsearch_index, updated_dummy_public_dataset_record, 1)
+        time.sleep(1)
+        result = search_index(es, settings.elasticsearch_index, dataset_query)
+        assert (
+            result.body["responses"][0]["hits"]["hits"][0]["_source"]["name"]
+            == "public test dataset 2"
+        )
+        delete_document_by_id(es, settings.elasticsearch_index, 1)
+        delete_index(es, settings.elasticsearch_index)
