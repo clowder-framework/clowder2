cb6ab6d928a082c01fb406a420cb8ff34e8d9670
Chen Wang <cwang138@illinois.edu>
2023-12-01 15:44:37 -0600

    populate manage users page

--- frontend/src/components/users/ManageUsers.tsx
+++ frontend/src/components/users/ManageUsers.tsx
@@ -9,49 +9,47 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { fetchUserProfile } from "../../actions/user";
+import { fetchAllUsers as fetchAllUsersAction } from "../../actions/user";
 
 export const ManageUsers = (): JSX.Element => {
 	const dispatch = useDispatch();
-	const user = useSelector((state: RootState) => state.user);
-	const profile = user["profile"];
-	const fetchProfile = () => dispatch(fetchUserProfile());
+	const users = useSelector((state: RootState) => state.group.users);
+	const fetchAllUsers = () => dispatch(fetchAllUsersAction());
 	// component did mount
 	useEffect(() => {
-		fetchProfile();
+		fetchAllUsers();
 	}, []);
 
-	if (profile != null) {
-		return (
-			<Layout>
-				<TableContainer component={Paper}>
-					<Table sx={{ minWidth: 650 }} aria-label="simple table">
-						<TableHead>
-							<TableRow>
-								<TableCell>Name</TableCell>
-								<TableCell align="right">Email</TableCell>
-								<TableCell align="right">Admin</TableCell>
-								<TableCell align="right" />
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							<TableRow
-								sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-							>
-								<TableCell>
-									{profile.first_name} {profile.last_name}
-								</TableCell>
-								<TableCell align="right">{profile.email}</TableCell>
-								<TableCell align="right">
-									{profile.admin ? "True" : "False"}
-								</TableCell>
-							</TableRow>
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</Layout>
-		);
-	} else {
-		return <p>nothing yet</p>;
-	}
+	return (
+		<Layout>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Name</TableCell>
+							<TableCell align="right">Email</TableCell>
+							<TableCell align="right">Admin</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{users.map((profile) => {
+							return (
+								<TableRow
+									sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+								>
+									<TableCell>
+										{profile.first_name} {profile.last_name}
+									</TableCell>
+									<TableCell align="right">{profile.email}</TableCell>
+									<TableCell align="right">
+										{profile.admin ? "True" : "False"}
+									</TableCell>
+								</TableRow>
+							);
+						})}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</Layout>
+	);
 };
--- frontend/src/components/users/Profile.tsx
+++ frontend/src/components/users/Profile.tsx
@@ -13,45 +13,39 @@ import { fetchUserProfile } from "../../actions/user";
 
 export const Profile = (): JSX.Element => {
 	const dispatch = useDispatch();
-	const user = useSelector((state: RootState) => state.user);
-	const profile = user["profile"];
+	const profile = useSelector((state: RootState) => state.user.profile);
 	const fetchProfile = () => dispatch(fetchUserProfile());
 	// component did mount
 	useEffect(() => {
 		fetchProfile();
 	}, []);
 
-	if (profile != null) {
-		return (
-			<Layout>
-				<TableContainer component={Paper}>
-					<Table sx={{ minWidth: 650 }} aria-label="simple table">
-						<TableHead>
-							<TableRow>
-								<TableCell>Name</TableCell>
-								<TableCell align="right">Email</TableCell>
-								<TableCell align="right">Admin</TableCell>
-								<TableCell align="right" />
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							<TableRow
-								sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-							>
-								<TableCell>
-									{profile.first_name} {profile.last_name}
-								</TableCell>
-								<TableCell align="right">{profile.email}</TableCell>
-								<TableCell align="right">
-									{profile.admin ? "True" : "False"}
-								</TableCell>
-							</TableRow>
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</Layout>
-		);
-	} else {
-		return <p>nothing yet</p>;
-	}
+	return (
+		<Layout>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Name</TableCell>
+							<TableCell align="right">Email</TableCell>
+							<TableCell align="right">Admin</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						<TableRow
+							sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+						>
+							<TableCell>
+								{profile.first_name} {profile.last_name}
+							</TableCell>
+							<TableCell align="right">{profile.email}</TableCell>
+							<TableCell align="right">
+								{profile.admin ? "True" : "False"}
+							</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</Layout>
+	);
 };
