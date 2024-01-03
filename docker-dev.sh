86c2b4abe9472f0547dfd85636084bf8ba8cc9df
Patrick Meade <blinkdog@protonmail.com>
2024-01-03 14:34:49 -0600

    Configure memory limits on ElasticSearch containers in development

--- docker-dev.sh
+++ docker-dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 if [ $1 == "up" ]
 then
-  docker-compose -f docker-compose.dev.yml -p clowder2-dev up -d
+  docker-compose --compatibility -f docker-compose.dev.yml -p clowder2-dev up -d
 fi
 if [ $1 == "down" ]
 then
