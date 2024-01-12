706e6eaecbc9b83709e10a827c3bd0d0f5bca0dc
Todd Nicholson <40038535+tcnichol@users.noreply.github.com>
2024-01-12 15:23:07 -0600

    hide files pagination on other tabs (#863)
    * putting the buttons prev and next for file in a conditional. we do not see that except files and extractors, where it makes sense
    
    * previous and next inside of the files list dab
    
    * fixing merge conflict
    
    * fixing conflicts with main

--- frontend/src/components/datasets/Dataset.tsx
+++ frontend/src/components/datasets/Dataset.tsx
@@ -1,15 +1,6 @@
 // lazy loading
 import React, { useEffect, useState } from "react";
-import {
-	Box,
-	Button,
-	ButtonGroup,
-	Grid,
-	Stack,
-	Tab,
-	Tabs,
-	Typography,
-} from "@mui/material";
+import {Box, Button, ButtonGroup, Grid, Stack, Tab, Tabs, Typography} from "@mui/material";
 import { useParams, useSearchParams } from "react-router-dom";
 import { RootState } from "../../types/data";
 import { useDispatch, useSelector } from "react-redux";
@@ -29,20 +20,14 @@ import { EditMetadata } from "../metadata/EditMetadata";
 import { MainBreadcrumbs } from "../navigation/BreadCrumb";
 import {
 	deleteDatasetMetadata as deleteDatasetMetadataAction,
-	fetchDatasetMetadata,
-	fetchMetadataDefinitions,
+	fetchDatasetMetadata, fetchMetadataDefinitions,
 	patchDatasetMetadata as patchDatasetMetadataAction,
 	postDatasetMetadata,
 } from "../../actions/metadata";
 import Layout from "../Layout";
 import { ActionsMenu } from "./ActionsMenu";
 import { DatasetDetails } from "./DatasetDetails";
-import {
-	ArrowBack,
-	ArrowForward,
-	FormatListBulleted,
-	InsertDriveFile,
-} from "@material-ui/icons";
+import {ArrowBack, ArrowForward, FormatListBulleted, InsertDriveFile} from "@material-ui/icons";
 import { Listeners } from "../listeners/Listeners";
 import AssessmentIcon from "@mui/icons-material/Assessment";
 import HistoryIcon from "@mui/icons-material/History";
@@ -65,7 +50,6 @@ export const Dataset = (): JSX.Element => {
 	// search parameters
 	const [searchParams] = useSearchParams();
 	const folderId = searchParams.get("folder");
-
 	// Redux connect equivalent
 	const dispatch = useDispatch();
 	const updateDatasetMetadata = (
@@ -84,25 +68,19 @@ export const Dataset = (): JSX.Element => {
 		dispatch(fetchFolderPath(folderId));
 	const listFilesInDataset = (
 		datasetId: string | undefined,
-		folderId: string | null,
-		skip: number | undefined,
-		limit: number | undefined
-	) => dispatch(fetchFilesInDataset(datasetId, folderId, skip, limit));
+		folderId: string | null
+		, skip: number | undefined, limit: number | undefined) => dispatch(fetchFilesInDataset(datasetId, folderId, skip, limit));
 	const listFoldersInDataset = (
 		datasetId: string | undefined,
 		parentFolder: string | null,
-		skip: number | undefined,
-		limit: number | undefined
+		skip: number | undefined, limit: number | undefined
 	) => dispatch(fetchFoldersInDataset(datasetId, parentFolder, skip, limit));
 	const listDatasetAbout = (datasetId: string | undefined) =>
 		dispatch(fetchDatasetAbout(datasetId));
 	const listDatasetMetadata = (datasetId: string | undefined) =>
 		dispatch(fetchDatasetMetadata(datasetId));
-	const getMetadatDefinitions = (
-		name: string | null,
-		skip: number,
-		limit: number
-	) => dispatch(fetchMetadataDefinitions(name, skip, limit));
+	const getMetadatDefinitions = (name:string|null, skip:number, limit:number) => dispatch(fetchMetadataDefinitions(name, skip,limit));
+
 
 	// mapStateToProps
 	const about = useSelector((state: RootState) => state.dataset.about);
@@ -110,7 +88,6 @@ export const Dataset = (): JSX.Element => {
 		(state: RootState) => state.dataset.datasetRole
 	);
 	const folderPath = useSelector((state: RootState) => state.folder.folderPath);
-	const adminMode = useSelector((state: RootState) => state.user.adminMode);
 
 	// state
 	const [selectedTabIndex, setSelectedTabIndex] = useState<number>(0);
@@ -118,6 +95,7 @@ export const Dataset = (): JSX.Element => {
 		React.useState<boolean>(false);
 	const [metadataRequestForms, setMetadataRequestForms] = useState({});
 
+	const [allowSubmit, setAllowSubmit] = React.useState<boolean>(false);
 	// Error msg dialog
 	const [errorOpen, setErrorOpen] = useState(false);
 	const [showForbiddenPage, setShowForbiddenPage] = useState(false);
@@ -132,15 +110,12 @@ export const Dataset = (): JSX.Element => {
 	const [prevDisabled, setPrevDisabled] = useState<boolean>(true);
 	const [nextDisabled, setNextDisabled] = useState<boolean>(false);
 	const filesInDataset = useSelector((state: RootState) => state.dataset.files);
-	const foldersInDataset = useSelector(
-		(state: RootState) => state.folder.folders
-	);
+	const foldersInDataset = useSelector((state: RootState) => state.folder.folders);
+
+
+	const metadataDefinitionList = useSelector((state: RootState) => state.metadata.metadataDefinitionList);
 
 	// component did mount list all files in dataset
-	useEffect(() => {
-		getMetadatDefinitions(null, 0, 100);
-	}, []);
-
 	useEffect(() => {
 		listFilesInDataset(datasetId, folderId, skip, limit);
 		listFoldersInDataset(datasetId, folderId, skip, limit);
@@ -149,17 +124,15 @@ export const Dataset = (): JSX.Element => {
 	}, [searchParams]);
 
 	useEffect(() => {
-		listFilesInDataset(datasetId, folderId, skip, limit);
-		listFoldersInDataset(datasetId, folderId, skip, limit);
-		listDatasetAbout(datasetId);
-		getFolderPath(folderId);
-	}, [adminMode]);
+		getMetadatDefinitions(null, 0, 100);
+	}, []);
 
 	useEffect(() => {
 		// disable flipping if reaches the last page
 		if (filesInDataset.length < limit && foldersInDataset.length < limit)
 			setNextDisabled(true);
-		else setNextDisabled(false);
+		else
+			setNextDisabled(false);
 	}, [filesInDataset]);
 
 	useEffect(() => {
@@ -195,7 +168,7 @@ export const Dataset = (): JSX.Element => {
 		setPaths(tmpPaths);
 	}, [about, folderPath]);
 
-	// for pagination keep flipping until the return dataset is less than the limit
+		// for pagination keep flipping until the return dataset is less than the limit
 	const previous = () => {
 		if (currPageNum - 1 >= 0) {
 			setSkip((currPageNum - 1) * limit);
@@ -335,7 +308,7 @@ export const Dataset = (): JSX.Element => {
 							{...a11yProps(3)}
 							disabled={false}
 						/>
-						{datasetRole.role !== undefined && datasetRole.role !== "viewer" ? (
+						{datasetRole.role !== undefined && datasetRole.role !== "viewer" ?
 							<Tab
 								icon={<BuildIcon />}
 								iconPosition="start"
@@ -343,10 +316,9 @@ export const Dataset = (): JSX.Element => {
 								label="Extract"
 								{...a11yProps(4)}
 								disabled={false}
-							/>
-						) : (
+							/> :
 							<></>
-						)}
+						}
 						<Tab
 							icon={<HistoryIcon />}
 							iconPosition="start"
@@ -355,7 +327,7 @@ export const Dataset = (): JSX.Element => {
 							{...a11yProps(5)}
 							disabled={false}
 						/>
-						{datasetRole.role !== undefined && datasetRole.role !== "viewer" ? (
+						{datasetRole.role !== undefined && datasetRole.role !== "viewer" ?
 							<Tab
 								icon={<ShareIcon />}
 								iconPosition="start"
@@ -363,10 +335,9 @@ export const Dataset = (): JSX.Element => {
 								label="Sharing"
 								{...a11yProps(6)}
 								disabled={false}
-							/>
-						) : (
+							/> :
 							<></>
-						)}
+						}
 					</Tabs>
 					<TabPanel value={selectedTabIndex} index={0}>
 						{folderId !== null ? (
@@ -377,14 +348,33 @@ export const Dataset = (): JSX.Element => {
 							<></>
 						)}
 						<FilesTable datasetId={datasetId} folderId={folderId} />
+						<Box display="flex" justifyContent="center" sx={{ m: 1 }}>
+							<ButtonGroup
+								variant="contained"
+								aria-label="previous next buttons"
+							>
+								<Button
+									aria-label="previous"
+									onClick={previous}
+									disabled={prevDisabled}
+								>
+									<ArrowBack /> Prev
+								</Button>
+								<Button
+									aria-label="next"
+									onClick={next}
+									disabled={nextDisabled}
+								>
+									Next <ArrowForward />
+								</Button>
+							</ButtonGroup>
+						</Box>
 					</TabPanel>
 					<TabPanel value={selectedTabIndex} index={1}>
 						<Visualization datasetId={datasetId} />
 					</TabPanel>
 					<TabPanel value={selectedTabIndex} index={2}>
-						{enableAddMetadata &&
-						datasetRole.role !== undefined &&
-						datasetRole.role !== "viewer" ? (
+						{enableAddMetadata && datasetRole.role !== undefined && datasetRole.role !== "viewer" ? (
 							<>
 								<EditMetadata
 									resourceType="dataset"
@@ -416,9 +406,7 @@ export const Dataset = (): JSX.Element => {
 									resourceId={datasetId}
 								/>
 								<Box textAlign="center">
-									{enableAddMetadata &&
-									datasetRole.role !== undefined &&
-									datasetRole.role !== "viewer" ? (
+									{enableAddMetadata && datasetRole.role !== undefined && datasetRole.role !== "viewer" ?
 										<Button
 											variant="contained"
 											sx={{ m: 2 }}
@@ -426,11 +414,10 @@ export const Dataset = (): JSX.Element => {
 												setEnableAddMetadata(true);
 											}}
 										>
-											Add Metadata
-										</Button>
-									) : (
+										Add Metadata
+										</Button> :
 										<></>
-									)}
+									}
 								</Box>
 							</>
 						)}
@@ -443,37 +430,21 @@ export const Dataset = (): JSX.Element => {
 							resourceId={datasetId}
 						/>
 					</TabPanel>
-					{datasetRole.role !== undefined && datasetRole.role !== "viewer" ? (
+					{datasetRole.role !== undefined && datasetRole.role !== "viewer" ?
 						<TabPanel value={selectedTabIndex} index={4}>
 							<Listeners datasetId={datasetId} />
-						</TabPanel>
-					) : (
+						</TabPanel> :
 						<></>
-					)}
+					}
 					<TabPanel value={selectedTabIndex} index={5}>
 						<ExtractionHistoryTab datasetId={datasetId} />
 					</TabPanel>
-					{datasetRole.role !== undefined && datasetRole.role !== "viewer" ? (
+					{datasetRole.role !== undefined && datasetRole.role !== "viewer" ?
 						<TabPanel value={selectedTabIndex} index={6}>
 							<SharingTab datasetId={datasetId} />
 						</TabPanel>
-					) : (
-						<></>
-					)}
-					<Box display="flex" justifyContent="center" sx={{ m: 1 }}>
-						<ButtonGroup variant="contained" aria-label="previous next buttons">
-							<Button
-								aria-label="previous"
-								onClick={previous}
-								disabled={prevDisabled}
-							>
-								<ArrowBack /> Prev
-							</Button>
-							<Button aria-label="next" onClick={next} disabled={nextDisabled}>
-								Next <ArrowForward />
-							</Button>
-						</ButtonGroup>
-					</Box>
+						: <></>
+					}
 				</Grid>
 				<Grid item>
 					<DatasetDetails details={about} />
