b76055de7a8413994a24afe8c226b919585753be
toddn <tcnichol@illinois.edu>
2024-02-29 14:51:59 -0600

    adding admin mode to the authwrapper

--- frontend/src/components/auth/AuthWrapper.tsx
+++ frontend/src/components/auth/AuthWrapper.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {RootState} from "../../types/data";
+
+
 
 export const AuthWrapper = (props): JSX.Element => {
 	const {currRole, allowedRoles, children} = props;
+	const adminMode = useSelector((state: RootState) => state.user.adminMode);
+
 	return (
 		<>
-			{allowedRoles.includes(currRole) ? children : <></>}
+			{(adminMode || allowedRoles.includes(currRole)) ? children : <></>}
 		</>
 	);
 }
