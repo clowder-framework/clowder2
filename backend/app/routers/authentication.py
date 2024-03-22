ed07791319144bf8d8da8257c1f843b80e8e3f9b
toddn <tcnichol@illinois.edu>
2024-03-22 14:30:10 -0500

    routes for enable/disable read only user

--- backend/app/routers/authentication.py
+++ backend/app/routers/authentication.py
@@ -201,3 +201,37 @@ async def revoke_admin(
             status_code=403,
             detail=f"User {current_username.email} is not an admin. Only admin can revoke admin access.",
         )
+
+@router.post("/users/enable_readonly/{useremail}", response_model=UserOut)
+async def enable_readonly_user(
+    useremail: str, current_username=Depends(get_current_user), admin=Depends(get_admin)
+):
+    if admin and current_username.admin:
+        if (user := await UserDB.find_one(UserDB.email == useremail)) is not None:
+            user.read_only_user = True
+            await user.replace()
+            return user.dict()
+        else:
+            raise HTTPException(status_code=404, detail=f"User {useremail} not found")
+    else:
+        raise HTTPException(
+            status_code=403,
+            detail=f"User {current_username.email} is not an admin. Only admin can make others admin.",
+        )
+
+@router.post("/users/set_readonly/{useremail}", response_model=UserOut)
+async def disable_readonly_user(
+    useremail: str, current_username=Depends(get_current_user), admin=Depends(get_admin)
+):
+    if admin and current_username.admin:
+        if (user := await UserDB.find_one(UserDB.email == useremail)) is not None:
+            user.read_only_user = False
+            await user.replace()
+            return user.dict()
+        else:
+            raise HTTPException(status_code=404, detail=f"User {useremail} not found")
+    else:
+        raise HTTPException(
+            status_code=403,
+            detail=f"User {current_username.email} is not an admin. Only admin can make others admin.",
+        )
