69731304a76cccb422e9937c126db7d6d7c71cd8
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2025-04-17 15:40:23 -0500

    Fix module not found warning.

--- frontend/src/types/global.d.ts
+++ frontend/src/types/global.d.ts
@@ -51,3 +51,7 @@ declare module "*.css" {
 	const classes: { readonly [key: string]: string };
 	export default classes;
 }
+
+declare module "mirador";
+declare module "mirador-annotations";
+declare module "mirador-annotations/lib/LocalStorageAdapter";
